918e0085a42ecf4005897574f2cbfc4544c9422f
Mario Danic <mario@lovelyhq.com>
2017-10-25 21:51:00 +0200

    Update copyright for email
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- app/src/main/java/com/nextcloud/talk/utils/preferences/AppPreferences.java
+++ app/src/main/java/com/nextcloud/talk/utils/preferences/AppPreferences.java
@@ -2,7 +2,7 @@
  * Nextcloud Talk application
  *
  * @author Mario Danic
- * Copyright (C) 2017 Mario Danic
+ * Copyright (C) 2017 Mario Danic <mario@lovelyhq.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
