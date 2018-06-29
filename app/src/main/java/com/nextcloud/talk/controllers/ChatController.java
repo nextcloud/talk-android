dce1ec6e6c7777b84a92fdd49c7856ea5deab455
Mario Danic <mario@lovelyhq.com>
2018-06-29 22:55:21 +0200

    Remove import
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- app/src/main/java/com/nextcloud/talk/controllers/ChatController.java
+++ app/src/main/java/com/nextcloud/talk/controllers/ChatController.java
@@ -75,7 +75,6 @@ import com.nextcloud.talk.models.json.rooms.RoomOverall;
 import com.nextcloud.talk.models.json.rooms.RoomsOverall;
 import com.nextcloud.talk.presenters.MentionAutocompletePresenter;
 import com.nextcloud.talk.utils.ApiUtils;
-import com.nextcloud.talk.utils.DisplayUtils;
 import com.nextcloud.talk.utils.KeyboardUtils;
 import com.nextcloud.talk.utils.bundle.BundleKeys;
 import com.nextcloud.talk.utils.database.user.UserUtils;
