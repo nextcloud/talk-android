f04e27ee6e721cc4af2824a4f164e5381beefe96
Mario Danic <mario@lovelyhq.com>
2019-02-21 10:13:13 +0100

    Fix an issue with conversation password not being applied to calls
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- app/src/main/java/com/nextcloud/talk/controllers/CallController.java
+++ app/src/main/java/com/nextcloud/talk/controllers/CallController.java
@@ -249,6 +249,7 @@ public class CallController extends BaseController {
     private WebSocketConnectionHelper webSocketConnectionHelper;
     private boolean hasMCU;
     private boolean hasExternalSignalingServer;
+    private String conversationPassword;
 
     public CallController(Bundle args) {
         super(args);
@@ -257,16 +258,13 @@ public class CallController extends BaseController {
         roomId = args.getString(BundleKeys.KEY_ROOM_ID, "");
         roomToken = args.getString(BundleKeys.KEY_ROOM_TOKEN, "");
         conversationUser = args.getParcelable(BundleKeys.KEY_USER_ENTITY);
-
-        if (conversationUser == null) {
-            conversationUser = userUtils.getCurrentUser();
-        }
-
-        credentials = ApiUtils.getCredentials(conversationUser.getUsername(), conversationUser.getToken());
+        conversationPassword = args.getString(BundleKeys.KEY_CONVERSATION_PASSWORD, "");
         isVoiceOnlyCall = args.getBoolean(BundleKeys.KEY_CALL_VOICE_ONLY, false);
 
         if (conversationUser.getUserId().equals("?")) {
             credentials = null;
+        } else {
+            credentials = ApiUtils.getCredentials(conversationUser.getUsername(), conversationUser.getToken());
         }
 
         baseUrl = args.getString(BundleKeys.KEY_MODIFIED_BASE_URL, "");
@@ -1035,7 +1033,8 @@ public class CallController extends BaseController {
     }
 
     private void joinRoomAndCall() {
-        ncApi.joinRoom(credentials, ApiUtils.getUrlForSettingMyselfAsActiveParticipant(baseUrl, roomToken), null)
+        ncApi.joinRoom(credentials, ApiUtils.getUrlForSettingMyselfAsActiveParticipant(baseUrl,
+                roomToken), conversationPassword)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .retry(3)
