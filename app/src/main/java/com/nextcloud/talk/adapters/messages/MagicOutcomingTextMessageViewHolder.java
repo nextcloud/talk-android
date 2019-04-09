1938c3fbb4ee5a686925f73b89f70e7a3c00a41a
Mario Danic <mario@lovelyhq.com>
2019-04-09 21:52:25 +0200

    Color changes to own mention
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- app/src/main/java/com/nextcloud/talk/adapters/messages/MagicOutcomingTextMessageViewHolder.java
+++ app/src/main/java/com/nextcloud/talk/adapters/messages/MagicOutcomingTextMessageViewHolder.java
@@ -105,7 +105,7 @@ public class MagicOutcomingTextMessageViewHolder extends MessageHolders.Outcomin
                                             messageString,
                                             individualHashMap.get("id"),
                                             individualHashMap.get("name"),
-                                            R.xml.chip_accent_background);
+                                            R.xml.chip_outgoing_own_mention);
                         }
                     } else if (individualHashMap.get("type").equals("file")) {
                         itemView.setOnClickListener(v -> {
