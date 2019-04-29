a2bb7cde41907da69d64d0210edeaedbbe20338c
Mario Danic <mario@lovelyhq.com>
2019-04-29 10:41:16 +0200

    Use fileID for passing to the files app
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- app/src/main/java/com/nextcloud/talk/utils/bundle/BundleKeys.java
+++ app/src/main/java/com/nextcloud/talk/utils/bundle/BundleKeys.java
@@ -54,5 +54,5 @@ public class BundleKeys {
     public static final String KEY_BROWSER_TYPE = "KEY_BROWSER_TYPE";
     public static final String KEY_FILE_PATHS = "KEY_FILE_PATHS";
     public static final String KEY_ACCOUNT = "KEY_ACCOUNT";
-    public static final String KEY_FILE_PATH = "KEY_FILE_PATH";
+    public static final String KEY_FILE_ID = "KEY_FILE_ID";
 }
