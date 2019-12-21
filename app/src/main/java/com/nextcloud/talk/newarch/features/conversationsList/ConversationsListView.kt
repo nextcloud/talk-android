87af803cc837496e72cb7879bd9fe4cb5e975d7a
Mario Danic <mario@lovelyhq.com>
2019-12-21 22:43:25 +0100

    Fixups due to wrong merge

--- app/src/main/java/com/nextcloud/talk/newarch/features/conversationsList/ConversationsListView.kt
+++ app/src/main/java/com/nextcloud/talk/newarch/features/conversationsList/ConversationsListView.kt
@@ -22,7 +22,6 @@ package com.nextcloud.talk.newarch.features.conversationsList
 
 import android.app.SearchManager
 import android.content.Context
-import android.content.pm.ShortcutManager
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
@@ -59,7 +58,6 @@ import com.nextcloud.talk.newarch.utils.Images
 import com.nextcloud.talk.utils.ApiUtils
 import com.nextcloud.talk.utils.ConductorRemapping
 import com.nextcloud.talk.utils.DisplayUtils
-import com.nextcloud.talk.newarch.utils.ShortcutService
 import com.nextcloud.talk.utils.animations.SharedElementTransition
 import com.nextcloud.talk.utils.bundle.BundleKeys
 import eu.davidea.flexibleadapter.FlexibleAdapter
@@ -235,7 +233,6 @@ class ConversationsListView : BaseView(), OnQueryTextListener,
                     )
                 }
 
-<<<<<<< HEAD
                 recyclerViewAdapter.updateDataSet(
                         newConversations as
                                 List<IFlexible<ViewHolder>>?, false
@@ -247,18 +244,6 @@ class ConversationsListView : BaseView(), OnQueryTextListener,
                 recyclerViewAdapter.filterItems(500)
             })
         }
-=======
-    val shortcutService = ShortcutService(
-            context.getSystemService<ShortcutManager>(ShortcutManager::class.java),
-            context,
-            this
-            //this.offlineRepository
-    );
-    shortcutService.registerShortcuts()
-
-    return super.onCreateView(inflater, container)
-  }
->>>>>>> feature/app-shortcuts
 
         viewState.observe(this@ConversationsListView, Observer { value ->
             when (value) {
