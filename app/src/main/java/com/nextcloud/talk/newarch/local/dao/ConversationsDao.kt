f84a0df532dda402a1b5f82961b7cfba73208c4c
Mario Danic <mario@lovelyhq.com>
2019-12-23 13:09:34 +0100

    Sort shortcuts by favorite as well

--- app/src/main/java/com/nextcloud/talk/newarch/local/dao/ConversationsDao.kt
+++ app/src/main/java/com/nextcloud/talk/newarch/local/dao/ConversationsDao.kt
@@ -30,7 +30,7 @@ abstract class ConversationsDao {
     @Query("SELECT * FROM conversations WHERE user_id = :userId ORDER BY favorite DESC, last_activity DESC")
     abstract fun getConversationsForUser(userId: Long): LiveData<List<ConversationEntity>>
 
-    @Query("SELECT * FROM conversations WHERE user_id = :userId ORDER BY last_activity DESC LIMIT 3")
+    @Query("SELECT * FROM conversations WHERE user_id = :userId ORDER BY favorite DESC, last_activity DESC LIMIT 3")
     abstract fun getLastThreeConversationsForUser(userId: Long): LiveData<List<ConversationEntity>>
 
     @Query("DELETE FROM conversations WHERE user_id = :userId")
