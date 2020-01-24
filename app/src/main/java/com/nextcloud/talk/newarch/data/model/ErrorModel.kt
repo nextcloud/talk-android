00abcd9c4852ffbf09a507da52cf326f46d8cf1b
Mario Danic <mario@lovelyhq.com>
2020-01-24 09:40:21 +0100

    Update copyright
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- app/src/main/java/com/nextcloud/talk/newarch/data/model/ErrorModel.kt
+++ app/src/main/java/com/nextcloud/talk/newarch/data/model/ErrorModel.kt
@@ -1,23 +1,23 @@
 /*
- * Nextcloud Talk application
  *
- * @author Mario Danic
- * Copyright (C) 2017-2019 Mario Danic <mario@lovelyhq.com>
+ *  * Nextcloud Talk application
+ *  *
+ *  * @author Mario Danic
+ *  * Copyright (C) 2017-2020 Mario Danic <mario@lovelyhq.com>
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Inspired by: https://github.com/ZahraHeydari/Android-Clean-Arch-Coroutines-Koin/blob/c63e62bf8fb9c099a983fcc7d1b5616a7318aa3f/app/src/main/java/com/android/post/data/model/ErrorModel.kt
  */
 
 package com.nextcloud.talk.newarch.data.model
