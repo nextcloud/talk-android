7ec6e67cf3a7ed0c9554dcc47b402db89bbb7298
Andy Scherzinger <info@andy-scherzinger.de>
2021-04-26 22:05:52 +0200

    add suffix to the apk name
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- scripts/uploadArtifact.sh
+++ scripts/uploadArtifact.sh
@@ -17,7 +17,7 @@ GITHUB_TOKEN=$5
 if ! test -e app/build/outputs/apk/qa/debug/app-qa-*.apk ; then
     exit 1
 fi
-echo "Uploaded artifact to $DAV_URL/$BUILD.apk"
+echo "Uploaded artifact to $DAV_URL/$BUILD-talk.apk"
 
 # delete all old comments, starting with "APK file:"
 oldComments=$(curl 2>/dev/null --header "authorization: Bearer $GITHUB_TOKEN" -X GET https://api.github.com/repos/nextcloud/talk-android/issues/$PR/comments | jq '.[] | (.id |tostring) + "|" + (.user.login | test("github-actions") | tostring) + "|" + (.body | test("APK file:.*") | tostring)'  | grep "true|true" | tr -d "\"" | cut -f1 -d"|")
@@ -28,8 +28,8 @@ done
 
 apt-get -y install qrencode
 
-qrencode -o $PR.png "$PUBLIC_URL/$BUILD.apk"
+qrencode -o $PR.png "$PUBLIC_URL/$BUILD-talk.apk"
 
-curl -u $USER:$PASS -X PUT $DAV_URL/$BUILD.apk --upload-file app/build/outputs/apk/qa/debug/app-qa-*.apk
-curl -u $USER:$PASS -X PUT $DAV_URL/$BUILD.png --upload-file $PR.png
-curl --header "authorization: Bearer $GITHUB_TOKEN" -X POST https://api.github.com/repos/nextcloud/talk-android/issues/$PR/comments -d "{ \"body\" : \"APK file: $PUBLIC_URL/$BUILD.apk <br/><br/> ![qrcode]($PUBLIC_URL/$BUILD.png) <br/><br/>To test this change/fix you can simply download above APK file and install and test it in parallel to your existing Nextcloud Talk app. \" }"
+curl -u $USER:$PASS -X PUT $DAV_URL/$BUILD-talk.apk --upload-file app/build/outputs/apk/qa/debug/app-qa-*.apk
+curl -u $USER:$PASS -X PUT $DAV_URL/$BUILD-talk.png --upload-file $PR.png
+curl --header "authorization: Bearer $GITHUB_TOKEN" -X POST https://api.github.com/repos/nextcloud/talk-android/issues/$PR/comments -d "{ \"body\" : \"APK file: $PUBLIC_URL/$BUILD-talk.apk <br/><br/> ![qrcode]($PUBLIC_URL/$BUILD-talk.png) <br/><br/>To test this change/fix you can simply download above APK file and install and test it in parallel to your existing Nextcloud Talk app. \" }"
