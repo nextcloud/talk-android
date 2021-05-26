14fc3c8146e566813120b233d9c77c0a4f803586
Andy Scherzinger <info@andy-scherzinger.de>
2021-05-26 18:13:21 +0200

    move back to User java entity definition
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

---
+++ app/src/main/java/com/nextcloud/talk/models/database/User.java
@@ -0,0 +1,66 @@
+/*
+ * Nextcloud Talk application
+ *
+ * @author Mario Danic
+ * Copyright (C) 2017-2018 Mario Danic <mario@lovelyhq.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.nextcloud.talk.models.database;
+
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.bluelinelabs.logansquare.LoganSquare;
+import com.nextcloud.talk.models.json.capabilities.Capabilities;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+
+import io.requery.Entity;
+import io.requery.Generated;
+import io.requery.Key;
+import io.requery.Persistable;
+
+@Entity
+public interface User extends Parcelable, Persistable, Serializable {
+    String TAG = "UserEntity";
+
+    @Key
+    @Generated
+    long getId();
+
+    String getUserId();
+
+    String getUsername();
+
+    String getBaseUrl();
+
+    String getToken();
+
+    String getDisplayName();
+
+    String getPushConfigurationState();
+
+    String getCapabilities();
+
+    String getClientCertificate();
+
+    String getExternalSignalingServer();
+
+    boolean getCurrent();
+
+    boolean getScheduledForDeletion();
+}
