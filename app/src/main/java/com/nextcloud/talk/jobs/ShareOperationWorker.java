5ec0f26afb8854188a0dca670dead2800ed1f04e
Joas Schilling <coding@schilljs.com>
2021-06-21 11:40:40 +0200

    add voice messages
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/jobs/ShareOperationWorker.java
+++ app/src/main/java/com/nextcloud/talk/jobs/ShareOperationWorker.java
@@ -21,25 +21,29 @@
 package com.nextcloud.talk.jobs;
 
 import android.content.Context;
-import androidx.annotation.NonNull;
-import androidx.work.Data;
-import androidx.work.Worker;
-import androidx.work.WorkerParameters;
-import autodagger.AutoInjector;
+import android.util.Log;
+
 import com.nextcloud.talk.api.NcApi;
 import com.nextcloud.talk.application.NextcloudTalkApplication;
 import com.nextcloud.talk.models.database.UserEntity;
 import com.nextcloud.talk.utils.ApiUtils;
 import com.nextcloud.talk.utils.bundle.BundleKeys;
 import com.nextcloud.talk.utils.database.user.UserUtils;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
-import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import javax.inject.Inject;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+import autodagger.AutoInjector;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 @AutoInjector(NextcloudTalkApplication.class)
 public class ShareOperationWorker extends Worker {
@@ -47,12 +51,14 @@ public class ShareOperationWorker extends Worker {
     UserUtils userUtils;
     @Inject
     NcApi ncApi;
+    private final String TAG = "ShareOperationWorker";
     private long userId;
     private UserEntity operationsUser;
     private String roomToken;
     private List<String> filesArray = new ArrayList<>();
     private String credentials;
     private String baseUrl;
+    private String metaData;
 
     public ShareOperationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -60,6 +66,7 @@ public class ShareOperationWorker extends Worker {
         Data data = workerParams.getInputData();
         userId = data.getLong(BundleKeys.INSTANCE.getKEY_INTERNAL_USER_ID(), 0);
         roomToken = data.getString(BundleKeys.INSTANCE.getKEY_ROOM_TOKEN());
+        metaData = data.getString(BundleKeys.INSTANCE.getKEY_META_DATA());
         Collections.addAll(filesArray, data.getStringArray(BundleKeys.INSTANCE.getKEY_FILE_PATHS()));
         operationsUser = userUtils.getUserWithId(userId);
         credentials = ApiUtils.getCredentials(operationsUser.getUsername(), operationsUser.getToken());
@@ -70,12 +77,14 @@ public class ShareOperationWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+
         for (int i = 0; i < filesArray.size(); i++) {
             ncApi.createRemoteShare(credentials,
                     ApiUtils.getSharingUrl(baseUrl),
                     filesArray.get(i),
                     roomToken,
-                    "10")
+                    "10",
+                     metaData)
                     .subscribeOn(Schedulers.io())
                     .blockingSubscribe(new Observer<Void>() {
                         @Override
@@ -90,7 +99,7 @@ public class ShareOperationWorker extends Worker {
 
                         @Override
                         public void onError(Throwable e) {
-
+                            Log.w(TAG, "error while creating RemoteShare", e);
                         }
 
                         @Override
