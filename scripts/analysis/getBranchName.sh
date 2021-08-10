28594cf18d58e32cb39bc09cdc6e148de5b8c55f
Marcel Hibbe <dev@mhibbe.de>
2021-08-10 14:32:51 +0200

    prepare stable branch for 12.2
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- scripts/analysis/getBranchName.sh
+++ scripts/analysis/getBranchName.sh
@@ -3,7 +3,7 @@
 # $1: username, $2: password/token, $3: pull request number
 
 if [ -z $3 ] ; then
-    echo "master";
+    echo "stable-12.2";
 else
-    curl 2>/dev/null -u $1:$2 https://api.github.com/repos/nextcloud/talk-android/pulls/$3 | grep \"ref\": | grep -v master | cut -d"\"" -f4
+    curl 2>/dev/null -u $1:$2 https://api.github.com/repos/nextcloud/talk-android/pulls/$3 | grep \"ref\": | grep -v stable-12.2 | cut -d"\"" -f4
 fi
