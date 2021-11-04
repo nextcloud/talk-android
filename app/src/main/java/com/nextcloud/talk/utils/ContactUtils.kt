4167cb63bf04fdc56bfd4e279614dbe4a83ff040
Marcel Hibbe <dev@mhibbe.de>
2021-11-04 15:05:48 +0100

    share contact from attachment dialog
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

---
+++ app/src/main/java/com/nextcloud/talk/utils/ContactUtils.kt
@@ -0,0 +1,34 @@
+package com.nextcloud.talk.utils
+
+import android.content.Context
+import android.provider.ContactsContract
+
+object ContactUtils {
+
+    fun getDisplayNameFromDeviceContact(context: Context, id: String?): String? {
+        var displayName: String? = null
+        val whereName =
+            ContactsContract.Data.MIMETYPE +
+                " = ? AND " +
+                ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID +
+                " = ?"
+        val whereNameParams = arrayOf(ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, id)
+        val nameCursor = context.contentResolver.query(
+            ContactsContract.Data.CONTENT_URI,
+            null,
+            whereName,
+            whereNameParams,
+            ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME
+        )
+        if (nameCursor != null) {
+            while (nameCursor.moveToNext()) {
+                displayName =
+                    nameCursor.getString(
+                        nameCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME)
+                    )
+            }
+            nameCursor.close()
+        }
+        return displayName
+    }
+}
