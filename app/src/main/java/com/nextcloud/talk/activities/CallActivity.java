89d0bcfc785c24fdf8623e78937d73150b5b624d
Marcel Hibbe <dev@mhibbe.de>
2021-11-09 13:35:29 +0100

    hide navigation bar if pip is not available
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/activities/CallActivity.java
+++ app/src/main/java/com/nextcloud/talk/activities/CallActivity.java
@@ -284,6 +284,14 @@ public class CallActivity extends BaseActivity {
         binding = CallActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if (!isPipModePossible()){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                                                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                                                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                                                 View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            binding.controllerCallLayout.setFitsSystemWindows(false);
+        }
+
         Bundle extras = getIntent().getExtras();
         roomId = extras.getString(BundleKeys.INSTANCE.getKEY_ROOM_ID(), "");
         roomToken = extras.getString(BundleKeys.INSTANCE.getKEY_ROOM_TOKEN(), "");
