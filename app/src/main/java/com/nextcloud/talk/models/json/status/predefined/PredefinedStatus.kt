19060bddbd57f946972a8fc2e00860c46bf7cc77
Marcel Hibbe <dev@mhibbe.de>
2022-02-08 11:11:39 +0100

    delete comments
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/models/json/status/predefined/PredefinedStatus.kt
+++ app/src/main/java/com/nextcloud/talk/models/json/status/predefined/PredefinedStatus.kt
@@ -21,33 +21,3 @@ data class PredefinedStatus(
     // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
     constructor() : this("id", "icon", "message", null)
 }
-
-
-
-
-
-// @Parcelize
-// @JsonObject
-// data class Status(
-//     @JsonField(name = ["userId"])
-//     var userId: String?,
-//     @JsonField(name = ["message"])
-//     var message: String?,
-//     /* TODO: Change to enum */
-//     @JsonField(name = ["messageId"])
-//     var messageId: String?,
-//     @JsonField(name = ["messageIsPredefined"])
-//     var messageIsPredefined: Boolean,
-//     @JsonField(name = ["icon"])
-//     var icon: String?,
-//     @JsonField(name = ["clearAt"])
-//     var clearAt: Long = 0,
-//     /* TODO: Change to enum */
-//     @JsonField(name = ["status"])
-//     var status: String = "offline",
-//     @JsonField(name = ["statusIsUserDefined"])
-//     var statusIsUserDefined: Boolean
-// ) : Parcelable {
-//     // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
-//     constructor() : this(null, null, null, false, null, 0, "offline", false)
-// }
