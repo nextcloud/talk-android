2884c6e01c02cdb40172a6941efa93a6a4a8799e
Andy Scherzinger <info@andy-scherzinger.de>
2022-03-24 18:33:13 +0100

    Migrate search models to kotlin
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

---
+++ app/src/main/java/com/nextcloud/talk/models/json/search/ContactsByNumberOCS.kt
@@ -0,0 +1,39 @@
+/*
+ * Nextcloud Talk application
+ *  
+ * @author Tobias Kaminsky
+ * @author Andy Scherzinger
+ * Copyright (C) 2022 Andy Scherzinger <info@andy-scherzinger.de>
+ * Copyright (C) 2020 Tobias Kaminsky <tobias.kaminsky@nextcloud.com>
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.nextcloud.talk.models.json.search
+
+import android.os.Parcelable
+import com.nextcloud.talk.models.json.generic.GenericOCS
+import com.bluelinelabs.logansquare.annotation.JsonField
+import com.bluelinelabs.logansquare.annotation.JsonObject
+import java.util.HashMap
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@JsonObject
+data class ContactsByNumberOCS(
+    @JsonField(name = ["data"])
+    var map: Map<String, String> = HashMap()
+) : GenericOCS(), Parcelable {
+    // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
+    constructor() : this(HashMap())
+}
