c3dd263b3fc5366bd6618b34dfe3f5ffd2bf9709
Andy Scherzinger <info@andy-scherzinger.de>
2022-05-09 15:24:10 +0200

    proper kotlin code formatting
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/ui/dialog/MessageActionsDialog.kt
+++ app/src/main/java/com/nextcloud/talk/ui/dialog/MessageActionsDialog.kt
@@ -83,8 +83,10 @@ class MessageActionsDialog(
                 currentConversation?.type != Conversation.ConversationType.ROOM_TYPE_ONE_TO_ONE_CALL
         )
         initMenuDeleteMessage(showMessageDeletionButton)
-        initMenuForwardMessage(ChatMessage.MessageType.REGULAR_TEXT_MESSAGE == message.getMessageType() &&
-            !(message.isDeletedCommentMessage || message.isDeleted))
+        initMenuForwardMessage(
+            ChatMessage.MessageType.REGULAR_TEXT_MESSAGE == message.getMessageType() &&
+                !(message.isDeletedCommentMessage || message.isDeleted)
+        )
         initMenuMarkAsUnread(
             message.previousMessageId > NO_PREVIOUS_MESSAGE_ID &&
                 ChatMessage.MessageType.SYSTEM_MESSAGE != message.getMessageType() &&
