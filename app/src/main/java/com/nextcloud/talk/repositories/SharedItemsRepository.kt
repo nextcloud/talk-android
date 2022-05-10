577357011e7d43c02b89b06eda408d32e7526bed
Tim Krüger <t@timkrueger.me>
2022-05-10 13:09:45 +0200

    Let repository not expose API responses
    Signed-off-by: Tim Krüger <t@timkrueger.me>

--- app/src/main/java/com/nextcloud/talk/repositories/SharedItemsRepository.kt
+++ app/src/main/java/com/nextcloud/talk/repositories/SharedItemsRepository.kt
@@ -1,5 +1,6 @@
 package com.nextcloud.talk.repositories
 
+import android.util.Log
 import autodagger.AutoInjector
 import com.nextcloud.talk.R
 import com.nextcloud.talk.api.NcApi
@@ -7,8 +8,8 @@ import com.nextcloud.talk.application.NextcloudTalkApplication
 import com.nextcloud.talk.application.NextcloudTalkApplication.Companion.sharedApplication
 import com.nextcloud.talk.models.database.UserEntity
 import com.nextcloud.talk.models.json.chat.ChatShareOverall
-import com.nextcloud.talk.models.json.chat.ChatShareOverviewOverall
 import com.nextcloud.talk.utils.ApiUtils
+import com.nextcloud.talk.viewmodels.SharedItemsViewModel
 import io.reactivex.Observable
 import retrofit2.Response
 import java.util.Locale
@@ -26,11 +27,11 @@ class SharedItemsRepository {
         sharedApplication!!.componentApplication.inject(this)
     }
 
-    fun media(type: SharedItemType): Observable<Response<ChatShareOverall>>? {
+    fun media(type: SharedItemType): Observable<SharedMediaItems>? {
         return media(type, null)
     }
 
-    fun media(type: SharedItemType, lastKnownMessageId: Int?): Observable<Response<ChatShareOverall>>? {
+    fun media(type: SharedItemType, lastKnownMessageId: Int?): Observable<SharedMediaItems>? {
         val credentials = ApiUtils.getCredentials(parameters!!.userName, parameters!!.userToken)
 
         return ncApi.getSharedItems(
@@ -39,24 +40,86 @@ class SharedItemsRepository {
             type.toString().lowercase(Locale.ROOT),
             lastKnownMessageId,
             BATCH_SIZE
+        ).map { map(it, type) }
+    }
+
+    private fun map(response: Response<ChatShareOverall>, type: SharedItemType): SharedMediaItems {
+
+        var chatLastGiven: Int? = null
+        val items = mutableMapOf<String, SharedItem>()
+
+        if (response.headers()["x-chat-last-given"] != null) {
+            chatLastGiven = response.headers()["x-chat-last-given"]!!.toInt()
+        }
+
+        val mediaItems = response.body()!!.ocs!!.data
+        if (mediaItems != null) {
+            for (it in mediaItems) {
+                if (it.value.messageParameters?.containsKey("file") == true) {
+                    val fileParameters = it.value.messageParameters!!["file"]!!
+
+                    val previewAvailable =
+                        "yes".equals(fileParameters["preview-available"]!!, ignoreCase = true)
+
+                    items[it.value.id] = SharedItem(
+                        fileParameters["id"]!!,
+                        fileParameters["name"]!!,
+                        fileParameters["size"]!!.toLong(),
+                        it.value.timestamp,
+                        fileParameters["path"]!!,
+                        fileParameters["link"]!!,
+                        fileParameters["mimetype"]!!,
+                        previewAvailable,
+                        previewLink(fileParameters["id"]),
+                        parameters!!.userEntity
+                    )
+                } else {
+                    Log.w(TAG, "location and deckcard are not yet supported")
+                }
+            }
+        }
+
+        val sortedMutableItems = items.toSortedMap().values.toList().reversed().toMutableList()
+        val moreItemsExisting = items.count() == SharedItemsViewModel.BATCH_SIZE
+
+        return SharedMediaItems(
+            type,
+            sortedMutableItems,
+            chatLastGiven,
+            moreItemsExisting,
+            authHeader()
         )
     }
 
-    fun availableTypes(): Observable<Response<ChatShareOverviewOverall>>? {
+    fun availableTypes(): Observable<Set<SharedItemType>> {
         val credentials = ApiUtils.getCredentials(parameters!!.userName, parameters!!.userToken)
 
         return ncApi.getSharedItemsOverview(
             credentials,
             ApiUtils.getUrlForChatSharedItemsOverview(1, parameters!!.baseUrl, parameters!!.roomToken),
             1
-        )
+        ).map {
+            val types = mutableSetOf<SharedItemType>()
+            val typeMap = it.body()!!.ocs!!.data!!
+            for (t in typeMap) {
+                if (t.value.isNotEmpty()) {
+                    try {
+                        types += SharedItemType.typeFor(t.key)
+                    } catch (e: IllegalArgumentException) {
+                        Log.w(TAG, "Server responds an unknown shared item type: ${t.key}")
+                    }
+                }
+            }
+
+            types.toSet()
+        }
     }
 
     fun authHeader(): Map<String, String> {
         return mapOf(Pair("Authorization", ApiUtils.getCredentials(parameters!!.userName, parameters!!.userToken)))
     }
 
-    fun previewLink(fileId: String?): String {
+    private fun previewLink(fileId: String?): String {
         return ApiUtils.getUrlForFilePreviewWithFileId(
             parameters!!.baseUrl,
             fileId,
@@ -74,5 +137,6 @@ class SharedItemsRepository {
 
     companion object {
         const val BATCH_SIZE: Int = 28
+        private val TAG = SharedItemsRepository::class.simpleName
     }
 }
