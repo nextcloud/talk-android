b27096eece823a0c60707d07723561a79c5fcba5
Andy Scherzinger <info@andy-scherzinger.de>
2022-05-16 23:13:12 +0200

    Migrate ParticipantsOCS to kotlin
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/models/json/participants/ParticipantsOverall.kt
+++ app/src/main/java/com/nextcloud/talk/models/json/participants/ParticipantsOverall.kt
@@ -27,17 +27,14 @@ package com.nextcloud.talk.models.json.participants
 import android.os.Parcelable
 import com.bluelinelabs.logansquare.annotation.JsonField
 import com.bluelinelabs.logansquare.annotation.JsonObject
-import com.nextcloud.talk.models.json.generic.GenericMeta
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @JsonObject
-data class ParticipantsOCS(
-    @JsonField(name = ["meta"])
-    var meta: GenericMeta?,
-    @JsonField(name = ["data"])
-    var data: List<Participant>? = null
+data class ParticipantsOverall(
+    @JsonField(name = ["ocs"])
+    var ocs: ParticipantsOCS? = null
 ) : Parcelable {
     // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
-    constructor() : this(null, null)
+    constructor() : this(null)
 }
