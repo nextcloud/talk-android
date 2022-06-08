41694b64bb986219b2f4b8c9003d18fc4943d8eb
Andy Scherzinger <info@andy-scherzinger.de>
2022-06-08 18:16:37 +0200

    fix copyright year in header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/remotefilebrowser/adapters/RemoteFileBrowserItemsListViewHolder.kt
+++ app/src/main/java/com/nextcloud/talk/remotefilebrowser/adapters/RemoteFileBrowserItemsListViewHolder.kt
@@ -2,7 +2,7 @@
  * Nextcloud Talk application
  *
  * @author Andy Scherzinger
- * Copyright (C) 202 Andy Scherzinger <info@andy-scherzinger.de>
+ * Copyright (C) 2022 Andy Scherzinger <info@andy-scherzinger.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- app/src/main/java/com/nextcloud/talk/remotefilebrowser/model/RemoteFileBrowserItem.kt
+++ app/src/main/java/com/nextcloud/talk/remotefilebrowser/model/RemoteFileBrowserItem.kt
@@ -3,7 +3,7 @@
  *
  * @author Andy Scherzinger
  * @author Mario Danic
- * Copyright (C) 202 Andy Scherzinger <info@andy-scherzinger.de>
+ * Copyright (C) 2022 Andy Scherzinger <info@andy-scherzinger.de>
  * Copyright (C) 2017-2018 Mario Danic <mario@lovelyhq.com>
  *
  * This program is free software: you can redistribute it and/or modify
