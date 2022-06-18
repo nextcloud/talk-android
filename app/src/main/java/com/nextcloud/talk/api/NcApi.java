205d00d443bc33e660de13a63fcefba829f94f75
Andy Scherzinger <info@andy-scherzinger.de>
2022-06-18 16:42:35 +0200

    harmonize license header in java/kt files
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/api/NcApi.java
+++ app/src/main/java/com/nextcloud/talk/api/NcApi.java
@@ -1,26 +1,25 @@
 /*
+ * Nextcloud Talk application
  *
- *   Nextcloud Talk application
+ * @author Mario Danic
+ * @author Marcel Hibbe
+ * @author Tim Krüger
+ * Copyright (C) 2017 Mario Danic (mario@lovelyhq.com)
+ * Copyright (C) 2021 Marcel Hibbe <dev@mhibbe.de>
+ * Copyright (C) 2021 Tim Krüger <t@timkrueger.me>
  *
- *   @author Mario Danic
- *   @author Marcel Hibbe
- *   @author Tim Krüger
- *   Copyright (C) 2017 Mario Danic (mario@lovelyhq.com)
- *   Copyright (C) 2021 Marcel Hibbe <dev@mhibbe.de>
- *   Copyright (C) 2021 Tim Krüger <t@timkrueger.me>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.nextcloud.talk.api;
 
