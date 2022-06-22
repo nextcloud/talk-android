3096d90bc6428ab6d1368568e9b8e3eb75df03e8
Andy Scherzinger <info@andy-scherzinger.de>
2022-06-22 11:21:46 +0200

    only set intent flag for sdk>=31
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/activities/CallActivity.java
+++ app/src/main/java/com/nextcloud/talk/activities/CallActivity.java
@@ -2606,12 +2606,22 @@ public class CallActivity extends CallBaseActivity {
             final ArrayList<RemoteAction> actions = new ArrayList<>();
 
             final Icon icon = Icon.createWithResource(this, iconId);
-            final PendingIntent intent =
-                PendingIntent.getBroadcast(
-                    this,
-                    requestCode,
-                    new Intent(MICROPHONE_PIP_INTENT_NAME).putExtra(MICROPHONE_PIP_INTENT_EXTRA_ACTION, requestCode),
-                    FLAG_MUTABLE);
+            PendingIntent intent;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                intent =
+                    PendingIntent.getBroadcast(
+                        this,
+                        requestCode,
+                        new Intent(MICROPHONE_PIP_INTENT_NAME).putExtra(MICROPHONE_PIP_INTENT_EXTRA_ACTION, requestCode),
+                        FLAG_MUTABLE);
+            } else {
+                intent =
+                    PendingIntent.getBroadcast(
+                        this,
+                        requestCode,
+                        new Intent(MICROPHONE_PIP_INTENT_NAME).putExtra(MICROPHONE_PIP_INTENT_EXTRA_ACTION, requestCode),
+                        0);
+            }
 
             actions.add(new RemoteAction(icon, title, title, intent));
 
