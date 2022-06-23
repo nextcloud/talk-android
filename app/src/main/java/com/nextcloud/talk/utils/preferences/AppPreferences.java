359104d6ba445546e2d2c2db19868c33c828f528
Andy Scherzinger <info@andy-scherzinger.de>
2022-06-23 18:25:51 +0200

    rename to isDbRoomMigrated and setDbRoomMigrated for proper kotlin interoperability
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/utils/preferences/AppPreferences.java
+++ app/src/main/java/com/nextcloud/talk/utils/preferences/AppPreferences.java
@@ -284,10 +284,10 @@ public interface AppPreferences {
 
     @KeyByString("db_cypher_v4_upgrade")
     @DefaultValue(R.bool.value_true)
-    boolean getIsDbCypherToUpgrade();
+    boolean isDbCypherToUpgrade();
 
     @KeyByString("db_cypher_v4_upgrade")
-    void setIsDbCypherToUpgrade(boolean value);
+    void setDbCypherToUpgrade(boolean value);
 
     @KeyByString("db_room_migrated")
     @DefaultValue(R.bool.value_false)
