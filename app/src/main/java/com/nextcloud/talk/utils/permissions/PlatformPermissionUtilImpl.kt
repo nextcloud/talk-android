baf0bc0b318d734413714d16ea037c3a30393639
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-07-12 13:16:35 +0200

    Make private broadcast permission depend on applicationId
    Otherwise we can't install QA and normal app side by side
    
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

---
+++ app/src/main/java/com/nextcloud/talk/utils/permissions/PlatformPermissionUtilImpl.kt
@@ -0,0 +1,44 @@
+/*
+ * Nextcloud Talk application
+ *
+ * @author Álvaro Brey
+ * Copyright (C) 2022 Álvaro Brey
+ * Copyright (C) 2022 Nextcloud GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.nextcloud.talk.utils.permissions
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.core.content.PermissionChecker
+import com.nextcloud.talk.BuildConfig
+
+class PlatformPermissionUtilImpl(private val context: Context) : PlatformPermissionUtil {
+    override val privateBroadcastPermission: String =
+        "${BuildConfig.APPLICATION_ID}.${BuildConfig.PERMISSION_LOCAL_BROADCAST}"
+
+    override fun isCameraPermissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return PermissionChecker.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PermissionChecker.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+}
