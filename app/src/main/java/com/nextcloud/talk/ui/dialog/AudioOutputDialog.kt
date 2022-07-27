0b201a7338b914351e327a77b1c4227ee9af86ea
Andy Scherzinger <info@andy-scherzinger.de>
2022-07-28 00:39:23 +0200

    fix ktlint issues
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/ui/dialog/AudioOutputDialog.kt
+++ app/src/main/java/com/nextcloud/talk/ui/dialog/AudioOutputDialog.kt
@@ -24,7 +24,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import autodagger.AutoInjector
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
