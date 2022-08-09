e321c02b2531e72bd86fd922134a336150187bf9
Joas Schilling <coding@schilljs.com>
2022-08-09 16:49:57 +0200

    Fix branch detection for drone PRs
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ scripts/analysis/getBranchName.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# $1: username, $2: password/token, $3: pull request number
+
+if [ -z $3 ] ; then
+    git branch | grep '*' | cut -d' ' -f2
+else
+    curl 2>/dev/null -u $1:$2 https://api.github.com/repos/nextcloud/talk-android/pulls/$3 | grep \"ref\": | grep -v master | cut -d"\"" -f4
+fi
