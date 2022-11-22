14c3bd19b653f44f08a0167c151388a40ee27b6a
Andy Scherzinger <info@andy-scherzinger.de>
2022-11-22 14:56:41 +0100

    Show conversation search results first
    Fixes #2504
    
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/controllers/ConversationsListController.kt
+++ app/src/main/java/com/nextcloud/talk/controllers/ConversationsListController.kt
@@ -1325,7 +1325,8 @@ class ConversationsListController(bundle: Bundle) :
                 if (results.hasMore) {
                     adapterItems.add(LoadMoreResultsItem)
                 }
-                adapter!!.addItems(0, adapterItems)
+                // add unified search result at the end of the list
+                adapter!!.addItems(adapter!!.mainItemCount + adapter!!.scrollableHeaders.size, adapterItems)
                 binding.recyclerView.scrollToPosition(0)
             }
         }
