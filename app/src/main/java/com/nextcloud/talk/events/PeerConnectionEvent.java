c8398695f4fa6554997b90967cb00b711157a48a
Daniel Calviño Sánchez <danxuliu@gmail.com>
2022-11-22 19:37:02 +0100

    Remove no longer needed code after removing EventBus message
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- app/src/main/java/com/nextcloud/talk/events/PeerConnectionEvent.java
+++ app/src/main/java/com/nextcloud/talk/events/PeerConnectionEvent.java
@@ -20,42 +20,15 @@
 
 package com.nextcloud.talk.events;
 
-import androidx.annotation.Nullable;
-
 public class PeerConnectionEvent {
     private final PeerConnectionEventType peerConnectionEventType;
-    private final String sessionId;
-    private final String nick;
-    private final Boolean changeValue;
-    private final String videoStreamType;
 
-    public PeerConnectionEvent(PeerConnectionEventType peerConnectionEventType, @Nullable String sessionId,
-                               @Nullable String nick, Boolean changeValue, @Nullable String videoStreamType) {
+    public PeerConnectionEvent(PeerConnectionEventType peerConnectionEventType) {
         this.peerConnectionEventType = peerConnectionEventType;
-        this.nick = nick;
-        this.changeValue = changeValue;
-        this.sessionId = sessionId;
-        this.videoStreamType = videoStreamType;
     }
 
     public PeerConnectionEventType getPeerConnectionEventType() {
         return this.peerConnectionEventType;
-    }
-
-    public String getSessionId() {
-        return this.sessionId;
-    }
-
-    public String getNick() {
-        return this.nick;
-    }
-
-    public Boolean getChangeValue() {
-        return this.changeValue;
-    }
-
-    public String getVideoStreamType() {
-        return this.videoStreamType;
     }
 
     public boolean equals(final Object o) {
@@ -74,25 +47,8 @@ public class PeerConnectionEvent {
         if (this$peerConnectionEventType == null ? other$peerConnectionEventType != null : !this$peerConnectionEventType.equals(other$peerConnectionEventType)) {
             return false;
         }
-        final Object this$sessionId = this.getSessionId();
-        final Object other$sessionId = other.getSessionId();
-        if (this$sessionId == null ? other$sessionId != null : !this$sessionId.equals(other$sessionId)) {
-            return false;
-        }
-        final Object this$nick = this.getNick();
-        final Object other$nick = other.getNick();
-        if (this$nick == null ? other$nick != null : !this$nick.equals(other$nick)) {
-            return false;
-        }
-        final Object this$changeValue = this.getChangeValue();
-        final Object other$changeValue = other.getChangeValue();
-        if (this$changeValue == null ? other$changeValue != null : !this$changeValue.equals(other$changeValue)) {
-            return false;
-        }
-        final Object this$videoStreamType = this.getVideoStreamType();
-        final Object other$videoStreamType = other.getVideoStreamType();
 
-        return this$videoStreamType == null ? other$videoStreamType == null : this$videoStreamType.equals(other$videoStreamType);
+        return true;
     }
 
     protected boolean canEqual(final Object other) {
@@ -104,22 +60,14 @@ public class PeerConnectionEvent {
         int result = 1;
         final Object $peerConnectionEventType = this.getPeerConnectionEventType();
         result = result * PRIME + ($peerConnectionEventType == null ? 43 : $peerConnectionEventType.hashCode());
-        final Object $sessionId = this.getSessionId();
-        result = result * PRIME + ($sessionId == null ? 43 : $sessionId.hashCode());
-        final Object $nick = this.getNick();
-        result = result * PRIME + ($nick == null ? 43 : $nick.hashCode());
-        final Object $changeValue = this.getChangeValue();
-        result = result * PRIME + ($changeValue == null ? 43 : $changeValue.hashCode());
-        final Object $videoStreamType = this.getVideoStreamType();
-        result = result * PRIME + ($videoStreamType == null ? 43 : $videoStreamType.hashCode());
         return result;
     }
 
     public String toString() {
-        return "PeerConnectionEvent(peerConnectionEventType=" + this.getPeerConnectionEventType() + ", sessionId=" + this.getSessionId() + ", nick=" + this.getNick() + ", changeValue=" + this.getChangeValue() + ", videoStreamType=" + this.getVideoStreamType() + ")";
+        return "PeerConnectionEvent(peerConnectionEventType=" + this.getPeerConnectionEventType() + ")";
     }
 
     public enum PeerConnectionEventType {
-        PEER_NEW, PEER_CHECKING, PEER_CONNECTED, PEER_COMPLETED, PEER_DISCONNECTED, PEER_FAILED, PEER_CLOSED, SENSOR_FAR, SENSOR_NEAR
+        SENSOR_FAR, SENSOR_NEAR
     }
 }
