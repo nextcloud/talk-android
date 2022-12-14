cbe89ff29f1c2226bf330256dcfeefdfb8ac36db
Marcel Hibbe <dev@mhibbe.de>
2022-12-14 15:50:07 +0100

    ensure that there is always a current user
    this should also fix the app to start after it always crashed on startup because of https://github.com/nextcloud/talk-android/issues/2531
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/users/UserManager.kt
+++ app/src/main/java/com/nextcloud/talk/users/UserManager.kt
@@ -43,6 +43,7 @@ class UserManager internal constructor(private val userRepository: UsersReposito
     val currentUser: Maybe<User>
         get() {
             return userRepository.getActiveUser()
+                .switchIfEmpty(getAnyUserAndSetAsActive())
         }
 
     val currentUserObservable: Observable<User>
