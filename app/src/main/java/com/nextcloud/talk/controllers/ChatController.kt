45f6e3b47426d6dd1fa6bd869adace8c3f839525
Andy Scherzinger <info@andy-scherzinger.de>
2023-02-08 12:30:21 +0100

    switch scrolling style to jump straight to "0"
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/controllers/ChatController.kt
+++ app/src/main/java/com/nextcloud/talk/controllers/ChatController.kt
@@ -673,7 +673,7 @@ class ChatController(args: Bundle) :
             }
         }
 
-        binding?.scrollDownButton?.setOnClickListener { binding?.messagesListView?.smoothScrollToPosition(0) }
+        binding?.scrollDownButton?.setOnClickListener { binding?.messagesListView?.scrollToPosition(0) }
         binding?.let { viewThemeUtils.material.colorMaterialButtonPrimaryTonal(it.scrollDownButton) }
 
         binding?.let { viewThemeUtils.material.colorMaterialButtonPrimaryFilled(it.popupBubbleView) }
