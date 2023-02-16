c55f9fdf79f18d3ff1ad5dda6a546f594054516a
Marcel Hibbe <dev@mhibbe.de>
2023-02-16 15:42:00 +0100

    Add/fix copyright headers
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/raisehand/RequestAssistanceRepository.kt
+++ app/src/main/java/com/nextcloud/talk/raisehand/RequestAssistanceRepository.kt
@@ -2,7 +2,7 @@
  * Nextcloud Talk application
  *
  * @author Marcel Hibbe
- * Copyright (C) 2022 Marcel Hibbe <dev@mhibbe.de>
+ * Copyright (C) 2023 Marcel Hibbe <dev@mhibbe.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
