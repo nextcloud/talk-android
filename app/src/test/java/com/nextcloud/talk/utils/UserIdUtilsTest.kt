27d977af1124d884da7dd85151615baf464f4a49
Smarshall <lcb2021048@iiitl.ac.in>
2023-08-27 02:34:43 +0530

    Added Unit Test For UserIdUtils

---
+++ app/src/test/java/com/nextcloud/talk/utils/UserIdUtilsTest.kt
@@ -0,0 +1,36 @@
+package com.nextcloud.talk.utils
+
+import com.nextcloud.talk.data.user.model.User
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class UserIdUtilsTest {
+
+    @Mock
+    private lateinit var user: User
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun testGetIdForUser_if_userIsNull_returnsNoId() {
+        Mockito.`when`(user.id).thenReturn(null)
+        val result = UserIdUtils.getIdForUser(user)
+        Assert.assertEquals("The id is NO_ID when user is null", UserIdUtils.NO_ID, result)
+    }
+
+    @Test
+    fun testGetIdForUser_if_userIdIsSet_returnsUserId() {
+        val expectedId: Long = 12345
+        Mockito.`when`(user.id).thenReturn(expectedId)
+        val result = UserIdUtils.getIdForUser(user)
+        Assert.assertEquals("The id is correct user id is not null", expectedId, result)
+    }
+}
+
