2cec1637e8199c0a11089c462fabd1dbb65cc151
Marcel Hibbe <dev@mhibbe.de>
2024-03-20 17:38:43 +0100

    hide call notification setting for federated rooms
    because right now it's response is http 406 as calls are not implemented for federation
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/conversationinfo/ConversationInfoActivity.kt
+++ app/src/main/java/com/nextcloud/talk/conversationinfo/ConversationInfoActivity.kt
@@ -199,7 +199,7 @@ class ConversationInfoActivity :
         if (databaseStorageModule == null) {
             databaseStorageModule = DatabaseStorageModule(conversationUser, conversationToken)
         }
-        setUpNotificationSettings(databaseStorageModule!!)
+
         binding.deleteConversationAction.setOnClickListener { showDeleteConversationDialog() }
         binding.leaveConversationAction.setOnClickListener { leaveConversation() }
         binding.clearConversationHistory.setOnClickListener { showClearHistoryDialog() }
@@ -689,6 +689,8 @@ class ConversationInfoActivity :
     @Suppress("LongMethod")
     private fun handleConversation() {
         val conversationCopy = conversation!!
+
+        setUpNotificationSettings(databaseStorageModule!!)
 
         if (CapabilitiesUtil.hasSpreedFeatureCapability(spreedCapabilities, SpreedFeatures.RICH_OBJECT_LIST_MEDIA) &&
             conversationCopy.remoteServer.isNullOrEmpty()
@@ -1347,8 +1349,13 @@ class ConversationInfoActivity :
         binding.notificationSettingsView.importantConversationSwitch.isChecked = module
             .getBoolean("important_conversation_switch", false)
 
-        binding.notificationSettingsView.callNotificationsSwitch.isChecked = module
-            .getBoolean("call_notifications_switch", true)
+        if (conversation!!.remoteServer.isNullOrEmpty()) {
+            binding.notificationSettingsView.notificationSettingsCallNotifications.visibility = VISIBLE
+            binding.notificationSettingsView.callNotificationsSwitch.isChecked = module
+                .getBoolean("call_notifications_switch", true)
+        } else {
+            binding.notificationSettingsView.notificationSettingsCallNotifications.visibility = GONE
+        }
     }
 
     companion object {
