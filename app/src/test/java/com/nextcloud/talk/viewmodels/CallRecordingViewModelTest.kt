bfcc6e9f86ac923bac3ab03f7088953c336432c0
Andy Scherzinger <info@andy-scherzinger.de>
2024-03-22 00:45:41 +0100

    Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/test/java/com/nextcloud/talk/viewmodels/CallRecordingViewModelTest.kt
+++ app/src/test/java/com/nextcloud/talk/viewmodels/CallRecordingViewModelTest.kt
@@ -1,3 +1,9 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2023 Marcel Hibbe <dev@mhibbe.de>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 package com.nextcloud.talk.viewmodels
 
 import com.nextcloud.talk.test.fakes.FakeCallRecordingRepository
