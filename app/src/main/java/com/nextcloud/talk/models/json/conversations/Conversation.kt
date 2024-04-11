nextcloud/talk-android

commit 754b825096c593014614f7062ede0a12d789e901

pass spreedCapabilities instead user to CapabilitiesUtil
To support federated rooms, capabilities have to be checked from the room which now also has capabilities.
If room is not federated, capabilities fromuser are still checked.
This is why CapabilitiesUtil had to be refactored to accept SpreedCapabilities which can come from room or user.

Other than that, many other changes were made as a result of this change.

Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

app/src/main/java/com/nextcloud/talk/models/json/conversations/Conversation.kt
@@ -38,8 +38,9 @@ import com.nextcloud.talk.models.json.converters.EnumParticipantTypeConverter
 import com.nextcloud.talk.models.json.converters.EnumReadOnlyConversationConverter
 import com.nextcloud.talk.models.json.converters.EnumRoomTypeConverter
 import com.nextcloud.talk.models.json.participants.Participant.ParticipantType
+import com.nextcloud.talk.utils.SpreedFeatures
 import com.nextcloud.talk.utils.ConversationUtils
-import com.nextcloud.talk.utils.database.user.CapabilitiesUtilNew
+import com.nextcloud.talk.utils.CapabilitiesUtil
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -160,7 +161,13 @@ data class Conversation(
     var callStartTime: Long? = null,
 
     @JsonField(name = ["recordingConsent"])
-    var recordingConsentRequired: Int = 0
+    var recordingConsentRequired: Int = 0,
+
+    @JsonField(name = ["remoteServer"])
+    var remoteServer: String? = null,
+
+    @JsonField(name = ["remoteToken"])
+    var remoteToken: String? = null
 
 ) : Parcelable {
     // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
@@ -185,13 +192,20 @@ data class Conversation(
     @Deprecated("Use ConversationUtil")
     private fun isLockedOneToOne(conversationUser: User): Boolean {
         return type == ConversationType.ROOM_TYPE_ONE_TO_ONE_CALL &&
-            CapabilitiesUtilNew.hasSpreedFeatureCapability(conversationUser, "locked-one-to-one-rooms")
+            CapabilitiesUtil.hasSpreedFeatureCapability(
+                conversationUser.capabilities?.spreedCapability!!,
+                SpreedFeatures.LOCKED_ONE_TO_ONE_ROOMS
+            )
     }
 
     @Deprecated("Use ConversationUtil")
     fun canModerate(conversationUser: User): Boolean {
         return isParticipantOwnerOrModerator &&
-            !isLockedOneToOne(conversationUser) &&
+            ConversationUtils.isLockedOneToOne(
+                ConversationModel.mapToConversationModel(this),
+                conversationUser
+                    .capabilities?.spreedCapability!!
+            ) &&
             type != ConversationType.FORMER_ONE_TO_ONE &&
             !ConversationUtils.isNoteToSelfConversation(ConversationModel.mapToConversationModel(this))
     }

commit d93dd0f3c10bc43e4468ced2d54c9c15c19ca1eb

fix logic for canModerate
bug was introduced with commit 754b8250

As a result, for example the delete conversation option was not shown.

Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

app/src/main/java/com/nextcloud/talk/models/json/conversations/Conversation.kt
@@ -185,10 +185,9 @@ data class Conversation(
     @Deprecated("Use ConversationUtil")
     fun canModerate(conversationUser: User): Boolean {
         return isParticipantOwnerOrModerator &&
-            ConversationUtils.isLockedOneToOne(
+            !ConversationUtils.isLockedOneToOne(
                 ConversationModel.mapToConversationModel(this),
-                conversationUser
-                    .capabilities?.spreedCapability!!
+                conversationUser.capabilities?.spreedCapability!!
             ) &&
             type != ConversationType.FORMER_ONE_TO_ONE &&
             !ConversationUtils.isNoteToSelfConversation(ConversationModel.mapToConversationModel(this))