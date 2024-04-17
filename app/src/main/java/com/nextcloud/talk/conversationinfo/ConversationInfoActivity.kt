3ea3f94cff8331e8ed62b908029d8d76d4db7d50
Marcel Hibbe <dev@mhibbe.de>
2024-04-17 14:40:01 +0200

    fix to show "Note to self" icon in conversation info
    share conversation link to other apps
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/conversationinfo/ConversationInfoActivity.kt
+++ app/src/main/java/com/nextcloud/talk/conversationinfo/ConversationInfoActivity.kt
@@ -958,10 +958,8 @@ class ConversationInfoActivity :
                 binding.avatarImage.loadSystemAvatar()
             }
 
-            ConversationType.DUMMY -> {
-                if (ConversationUtils.isNoteToSelfConversation(conversation!!)) {
-                    binding.avatarImage.loadNoteToSelfAvatar()
-                }
+            ConversationType.NOTE_TO_SELF -> {
+                binding.avatarImage.loadNoteToSelfAvatar()
             }
 
             else -> {
