24533cc11eb14d20b564f223ac1b7f0b44333ff0
sowjanyakch <sowjanya.kch@gmail.com>
2024-05-22 16:09:12 +0200

    Refactoring
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

---
+++ app/src/main/java/com/nextcloud/talk/api/NcAPI.kt
@@ -0,0 +1,44 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.api
+
+import com.nextcloud.talk.models.json.conversations.RoomOverall
+import kotlinx.coroutines.flow.Flow
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+import retrofit2.http.Url
+
+interface NcAPI{
+    @GET
+    fun getContactsWithSearchParam(
+        @Header("Authorization") authorization: String?,
+        @Url url: String?,
+        @Query("shareTypes[]") listOfShareTypes: List<String?>?,
+        @QueryMap options: Map<String?, Any?>
+    ): Flow<ResponseBody?>
+
+    /*
+        QueryMap items are as follows:
+            - "roomType" : ""
+            - "invite" : ""
+
+        Server URL is: baseUrl + ocsApiVersion + spreedApiVersion + /room
+     */
+    @POST
+    fun createRoom(
+        @Header("Authorization") authorization: String?,
+        @Url url: String?,
+        @QueryMap options: Map<String?, String?>
+    ): Flow<RoomOverall?>
+}
+
+
---
+++ app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityViewModel.kt
@@ -0,0 +1,20 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.contacts
+
+import androidx.lifecycle.ViewModel
+import com.nextcloud.talk.api.NcAPI
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactsActivityViewModel @Inject constructor(private val api:NcAPI):ViewModel(){
+
+
+
+}
