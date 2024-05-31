6ca2ba799478b2477abab920ebdcf59326759e7a
Marcel Hibbe <dev@mhibbe.de>
2024-05-31 12:43:44 +0200

    avoid "Please check your internet connection"
    avoid "Please check your internet connection" error when connected via ethernet or vpn
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/conversationlist/ConversationsListActivity.kt
+++ app/src/main/java/com/nextcloud/talk/conversationlist/ConversationsListActivity.kt
@@ -883,8 +883,10 @@ class ConversationsListActivity :
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+        return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
     }
 
     private fun sortConversations(conversationItems: MutableList<AbstractFlexibleItem<*>>) {
