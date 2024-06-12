162827bc7f264855aa6a61d438bc0684737002e7
sowjanyakch <sowjanya.kch@gmail.com>
2024-06-12 23:45:29 +0200

    Coroutines implementation
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityViewModel.kt
+++ app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityViewModel.kt
@@ -7,15 +7,47 @@
 
 package com.nextcloud.talk.contacts
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.nextcloud.talk.adapters.items.ContactItem
+import androidx.lifecycle.viewModelScope
 import com.nextcloud.talk.api.NcApiCoroutines
+import com.nextcloud.talk.models.json.autocomplete.AutocompleteUser
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class ContactsActivityViewModel @Inject constructor(private val ncApiCoroutines: NcApiCoroutines) : ViewModel()
+class ContactsActivityViewModel @Inject constructor(private val ncApiCoroutines: NcApiCoroutines) : ViewModel() {
+
+    var contactsViewState by mutableStateOf<ContactsUiState>(ContactsUiState.None)
+
+    fun getContactsFromSearchParams(
+        baseUrl: String,
+        ocsApiVersion: String,
+        shareList: List<String>,
+        options: Map<String, Any>
+    )  {
+        contactsViewState = ContactsUiState.Loading
+        viewModelScope.launch {
+            try {
+                val contacts = ncApiCoroutines.getContactsWithSearchParam(
+                    baseUrl,
+                    ocsApiVersion,
+                    shareList,
+                    options
+                )
+                val contactsList: List<AutocompleteUser>? = contacts.data
+            } catch (exception: Exception) {
+                exception.message?.let { ContactsUiState.Error(it) }
+            }
+        }
+    }
+}
 
 sealed class ContactsUiState {
+
+    object None : ContactsUiState()
     object Loading : ContactsUiState()
-    data class Success(val contacts: List<ContactItem>) : ContactsUiState()
+    data class Success(val contacts: List<AutocompleteUser>?) : ContactsUiState()
     data class Error(val message: String) : ContactsUiState()
 }
