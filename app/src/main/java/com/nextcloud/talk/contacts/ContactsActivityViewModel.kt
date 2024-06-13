357328e7f33a18b88def67bca563396e2adb13a0
sowjanyakch <sowjanya.kch@gmail.com>
2024-06-13 23:16:17 +0200

    Add contactsViewState
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityViewModel.kt
+++ app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityViewModel.kt
@@ -7,27 +7,27 @@
 
 package com.nextcloud.talk.contacts
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nextcloud.talk.api.NcApiCoroutines
 import com.nextcloud.talk.models.json.autocomplete.AutocompleteUser
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ContactsActivityViewModel @Inject constructor(private val ncApiCoroutines: NcApiCoroutines) : ViewModel() {
 
-    var contactsViewState by mutableStateOf<ContactsUiState>(ContactsUiState.None)
+    private val _contactsViewState = MutableStateFlow<ContactsUiState>(ContactsUiState.None)
+    val contactsViewState: StateFlow<ContactsUiState> = _contactsViewState
 
     fun getContactsFromSearchParams(
         baseUrl: String,
         ocsApiVersion: String,
         shareList: List<String>,
         options: Map<String, Any>
-    )  {
-        contactsViewState = ContactsUiState.Loading
+    ) {
+        _contactsViewState.value = ContactsUiState.Loading
         viewModelScope.launch {
             try {
                 val contacts = ncApiCoroutines.getContactsWithSearchParam(
@@ -37,8 +37,9 @@ class ContactsActivityViewModel @Inject constructor(private val ncApiCoroutines:
                     options
                 )
                 val contactsList: List<AutocompleteUser>? = contacts.data
+                _contactsViewState.value = ContactsUiState.Success(contactsList)
             } catch (exception: Exception) {
-                exception.message?.let { ContactsUiState.Error(it) }
+                _contactsViewState.value = ContactsUiState.Error(exception.message ?: "")
             }
         }
     }
