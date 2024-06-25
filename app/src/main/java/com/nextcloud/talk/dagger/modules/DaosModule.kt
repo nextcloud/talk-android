93085bfa0a88abed6bdacf02828e5b9a80d8ce5f
Marcel Hibbe <dev@mhibbe.de>
2024-06-25 11:40:44 +0200

    add DaosModule to inject ConversationsDao and ChatMessagesDao Signed-off-by: Marcel Hibbe <dev@mhibbe.de>
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

---
+++ app/src/main/java/com/nextcloud/talk/dagger/modules/DaosModule.kt
@@ -0,0 +1,28 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.dagger.modules
+
+import com.nextcloud.talk.data.database.dao.ChatMessagesDao
+import com.nextcloud.talk.data.database.dao.ConversationsDao
+import com.nextcloud.talk.data.source.local.TalkDatabase
+import dagger.Module
+import dagger.Provides
+
+@Module
+internal object DaosModule {
+    @Provides
+    fun providesConversationsDao(
+        database: TalkDatabase,
+    ): ConversationsDao = database.conversationsDao()
+
+    @Provides
+    fun providesChatDao(
+        database: TalkDatabase,
+    ): ChatMessagesDao = database.chatMessagesDao()
+}
+
