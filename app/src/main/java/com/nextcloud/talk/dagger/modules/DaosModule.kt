bc6967a392c9ac962ab51366176546a6471dc269
Marcel Hibbe <dev@mhibbe.de>
2024-06-25 11:53:29 +0200

    add copyright
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/dagger/modules/DaosModule.kt
+++ app/src/main/java/com/nextcloud/talk/dagger/modules/DaosModule.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud Talk - Android Client
  *
- * SPDX-FileCopyrightText: 2024 Your Name <your@email.com>
+ * SPDX-FileCopyrightText: 2024 Marcel Hibbe <dev@mhibbe.de>
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
