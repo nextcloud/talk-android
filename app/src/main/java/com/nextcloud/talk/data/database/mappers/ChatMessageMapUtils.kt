9ae0d524c0f1aaa674c56ad54b8bba2ea2a1e696
sowjanyakch <sowjanya.kch@gmail.com>
2024-08-13 14:41:01 +0200

    update ContactsViewModelTest - shareTypes
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/data/database/mappers/ChatMessageMapUtils.kt
+++ app/src/main/java/com/nextcloud/talk/data/database/mappers/ChatMessageMapUtils.kt
@@ -7,9 +7,9 @@
 
 package com.nextcloud.talk.data.database.mappers
 
-import com.nextcloud.talk.chat.data.model.ChatMessage
-import com.nextcloud.talk.data.database.model.ChatMessageEntity
 import com.nextcloud.talk.models.json.chat.ChatMessageJson
+import com.nextcloud.talk.data.database.model.ChatMessageEntity
+import com.nextcloud.talk.chat.data.model.ChatMessage
 
 fun ChatMessageJson.asEntity(accountId: Long) =
     ChatMessageEntity(
