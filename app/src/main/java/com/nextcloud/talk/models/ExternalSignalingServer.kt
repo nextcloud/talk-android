7cd9a3ffb787c2fcf2b8a388743b9c83d24d8e81
Daniel Calviño Sánchez <danxuliu@gmail.com>
2024-08-28 08:16:09 +0200

    Store federation properties in signaling settings
    Starting with Talk 20 the signaling settings include a "federation"
    property that provide the values needed to join a federated room in the
    external signaling settings. The "federation" property is specific to
    each conversation, and it will be returned although empty for
    non-federated conversations.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- app/src/main/java/com/nextcloud/talk/models/ExternalSignalingServer.kt
+++ app/src/main/java/com/nextcloud/talk/models/ExternalSignalingServer.kt
@@ -10,6 +10,7 @@ package com.nextcloud.talk.models
 import android.os.Parcelable
 import com.bluelinelabs.logansquare.annotation.JsonField
 import com.bluelinelabs.logansquare.annotation.JsonObject
+import com.nextcloud.talk.models.json.signaling.settings.FederationSettings
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -18,8 +19,10 @@ data class ExternalSignalingServer(
     @JsonField(name = ["externalSignalingServer"])
     var externalSignalingServer: String? = null,
     @JsonField(name = ["externalSignalingTicket"])
-    var externalSignalingTicket: String? = null
+    var externalSignalingTicket: String? = null,
+    @JsonField(name = ["federation"])
+    var federation: FederationSettings? = null
 ) : Parcelable {
     // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
-    constructor() : this(null, null)
+    constructor() : this(null, null, null)
 }
