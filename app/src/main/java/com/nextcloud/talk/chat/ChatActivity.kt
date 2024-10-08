aaa27ba0efcbc72cfd344a4fa07e742a8fec7fd0
sowjanyakch <sowjanya.kch@gmail.com>
2024-10-08 22:55:32 +0200

    modify previousChatMessageIndex
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -2666,8 +2666,7 @@ class ChatActivity :
             chatMessage.activeUser = conversationUser
 
             adapter?.let {
-                val lastItemIndex = adapter!!.itemCount - 1
-                val previousChatMessage = adapter!!.items[lastItemIndex].item as ChatMessage
+                val previousChatMessage = adapter!!.items[1].item as ChatMessage
                 chatMessage.isGrouped = groupMessages(chatMessage, previousChatMessage)
 
                 chatMessage.isOneToOneConversation =
