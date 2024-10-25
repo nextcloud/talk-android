9537a8d9d39ed5ff00ff68a17b8151a01e894a9a
sowjanyakch <sowjanya.kch@gmail.com>
2024-10-25 17:49:54 +0200

    add bundle keys for one-one conversation
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -167,6 +167,7 @@ import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_FILE_PATHS
 import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_INTERNAL_USER_ID
 import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_IS_BREAKOUT_ROOM
 import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_IS_MODERATOR
+import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_IS_ROOM_ONE_TO_ONE
 import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_RECORDING_STATE
 import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_ROOM_TOKEN
 import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_START_CALL_AFTER_ROOM_SWITCH
@@ -3072,6 +3073,7 @@ class ChatActivity :
                 BundleKeys.KEY_PARTICIPANT_PERMISSION_CAN_PUBLISH_AUDIO,
                 participantPermissions.canPublishAudio()
             )
+            bundle.putBoolean(KEY_IS_ROOM_ONE_TO_ONE, ConversationUtils.isOneToOne(it))
             bundle.putBoolean(
                 BundleKeys.KEY_PARTICIPANT_PERMISSION_CAN_PUBLISH_VIDEO,
                 participantPermissions.canPublishVideo()
--- app/src/main/java/com/nextcloud/talk/utils/ConversationUtils.kt
+++ app/src/main/java/com/nextcloud/talk/utils/ConversationUtils.kt
@@ -24,6 +24,10 @@ object ConversationUtils {
             Participant.ParticipantType.USER_FOLLOWING_LINK == conversation.participantType
     }
 
+    fun isOneToOne(conversation:ConversationModel):Boolean{
+        return ConversationEnums.ConversationType.ROOM_TYPE_ONE_TO_ONE_CALL == conversation.type
+    }
+
     fun isParticipantOwnerOrModerator(conversation: ConversationModel): Boolean {
         return Participant.ParticipantType.OWNER == conversation.participantType ||
             Participant.ParticipantType.GUEST_MODERATOR == conversation.participantType ||
@@ -59,4 +63,5 @@ object ConversationUtils {
         return currentConversation != null &&
             currentConversation.type == ConversationEnums.ConversationType.NOTE_TO_SELF
     }
+
 }
