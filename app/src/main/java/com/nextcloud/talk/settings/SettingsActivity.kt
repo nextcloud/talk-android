44624691c789434864203edc09483f0b03540997
Marcel Hibbe <dev@mhibbe.de>
2024-11-22 17:23:05 +0100

    Add permanent hint when battery optimization is not ignored
    Also add an option to not show the hint, when user doesn't want to ignore battery optimization.
    
    Only show these features when gplay services are available. Otherwise don't show them as it would pretend notifications would be possible without gplay.
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/settings/SettingsActivity.kt
+++ app/src/main/java/com/nextcloud/talk/settings/SettingsActivity.kt
@@ -663,6 +663,7 @@ class SettingsActivity : BaseActivity(), SetPhoneNumberDialogFragment.SetPhoneNu
     private fun themeSwitchPreferences() {
         binding.run {
             listOf(
+                settingsShowIgnoreBatteryOptimizationHintSwitch,
                 settingsScreenLockSwitch,
                 settingsScreenSecuritySwitch,
                 settingsIncognitoKeyboardSwitch,
@@ -858,6 +859,19 @@ class SettingsActivity : BaseActivity(), SetPhoneNumberDialogFragment.SetPhoneNu
     }
 
     private fun setupCheckables() {
+        binding.settingsShowIgnoreBatteryOptimizationHintSwitch.isChecked =
+            appPreferences.showIgnoreBatteryOptimizationHint
+
+        if (ClosedInterfaceImpl().isGooglePlayServicesAvailable) {
+            binding.settingsShowIgnoreBatteryOptimizationHint.setOnClickListener {
+                val isChecked = binding.settingsShowIgnoreBatteryOptimizationHintSwitch.isChecked
+                binding.settingsShowIgnoreBatteryOptimizationHintSwitch.isChecked = !isChecked
+                appPreferences.setShowIgnoreBatteryOptimizationHint(!isChecked)
+            }
+        } else {
+            binding.settingsShowIgnoreBatteryOptimizationHint.visibility = View.GONE
+        }
+
         binding.settingsScreenSecuritySwitch.isChecked = appPreferences.isScreenSecured
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
