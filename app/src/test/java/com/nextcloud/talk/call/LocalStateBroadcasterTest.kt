fe32bc1628ff81958c4d2e7cf6ed4c91dca049bc
Daniel Calviño Sánchez <danxuliu@gmail.com>
2024-11-30 06:25:26 +0100

    Add helper class to broadcast the local participant state
    The LocalStateBroadcaster observes changes in the
    LocalCallParticipantModel and notifies other participants in the call as
    needed. Although it is created right before joining the call there is a
    slim chance of the state changing before the local participant is
    actually in the call, but even in that case other participants would not
    be notified about the state due to the MessageSender depending on the
    list of call participants / peer connections passed to it, which should
    not be initialized before the local participant is actually in the call.
    
    There is, however, a race condition that could cause participants to not
    be added to the participant list if they join at the same time as the
    local participant and a signaling message listing them but not the local
    participant as in the call is received once the CallParticipantList was
    created, but that is unrelated to the broadcaster and will be fixed
    in another commit.
    
    Currently only changes in the audio, speaking and video state are
    notified, although in the future it should also notify about the nick,
    the raised hand or any other state (but not one-time events, like
    reactions). The notifications right now are sent only through data
    channels, but at a later point they will be sent also through signaling
    messages as needed.
    
    Similarly, although right now it only notifies of changes in the state
    it will also take care of notifying other participants about the current
    state when they join the call (or the local participant joins).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

---
+++ app/src/test/java/com/nextcloud/talk/call/LocalStateBroadcasterTest.kt
@@ -0,0 +1,260 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Daniel Calviño Sánchez <danxuliu@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+package com.nextcloud.talk.call
+
+import com.nextcloud.talk.models.json.signaling.DataChannelMessage
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+@Suppress("TooManyFunctions")
+class LocalStateBroadcasterTest {
+
+    private var localCallParticipantModel: MutableLocalCallParticipantModel? = null
+    private var mockedMessageSender: MessageSender? = null
+
+    private var localStateBroadcaster: LocalStateBroadcaster? = null
+
+    @Before
+    fun setUp() {
+        localCallParticipantModel = MutableLocalCallParticipantModel()
+        mockedMessageSender = Mockito.mock(MessageSender::class.java)
+    }
+
+    @Test
+    fun testEnableAudio() {
+        localCallParticipantModel!!.isAudioEnabled = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isAudioEnabled = true
+
+        val expectedAudioOn = DataChannelMessage("audioOn")
+
+        Mockito.verify(mockedMessageSender!!).sendToAll(expectedAudioOn)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableAudioTwice() {
+        localCallParticipantModel!!.isAudioEnabled = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isAudioEnabled = true
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableAudio() {
+        localCallParticipantModel!!.isAudioEnabled = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isAudioEnabled = false
+
+        val expectedAudioOff = DataChannelMessage("audioOff")
+
+        Mockito.verify(mockedMessageSender!!).sendToAll(expectedAudioOff)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableAudioTwice() {
+        localCallParticipantModel!!.isAudioEnabled = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isAudioEnabled = false
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableSpeaking() {
+        localCallParticipantModel!!.isAudioEnabled = true
+        localCallParticipantModel!!.isSpeaking = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = true
+
+        val expectedSpeaking = DataChannelMessage("speaking")
+
+        Mockito.verify(mockedMessageSender!!).sendToAll(expectedSpeaking)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableSpeakingTwice() {
+        localCallParticipantModel!!.isAudioEnabled = true
+        localCallParticipantModel!!.isSpeaking = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = true
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableSpeakingWithAudioDisabled() {
+        localCallParticipantModel!!.isAudioEnabled = false
+        localCallParticipantModel!!.isSpeaking = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = true
+
+        Mockito.verifyNoInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableAudioWhileSpeaking() {
+        localCallParticipantModel!!.isAudioEnabled = false
+        localCallParticipantModel!!.isSpeaking = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = true
+        localCallParticipantModel!!.isAudioEnabled = true
+
+        val expectedAudioOn = DataChannelMessage("audioOn")
+        val expectedSpeaking = DataChannelMessage("speaking")
+
+        val inOrder = Mockito.inOrder(mockedMessageSender)
+
+        inOrder.verify(mockedMessageSender!!).sendToAll(expectedAudioOn)
+        inOrder.verify(mockedMessageSender!!).sendToAll(expectedSpeaking)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableSpeaking() {
+        localCallParticipantModel!!.isAudioEnabled = true
+        localCallParticipantModel!!.isSpeaking = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = false
+
+        val expectedStoppedSpeaking = DataChannelMessage("stoppedSpeaking")
+
+        Mockito.verify(mockedMessageSender!!).sendToAll(expectedStoppedSpeaking)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableSpeakingTwice() {
+        localCallParticipantModel!!.isAudioEnabled = true
+        localCallParticipantModel!!.isSpeaking = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = false
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableAudioWhileSpeaking() {
+        localCallParticipantModel!!.isAudioEnabled = true
+        localCallParticipantModel!!.isSpeaking = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isAudioEnabled = false
+
+        val expectedStoppedSpeaking = DataChannelMessage("stoppedSpeaking")
+        val expectedAudioOff = DataChannelMessage("audioOff")
+
+        val inOrder = Mockito.inOrder(mockedMessageSender)
+
+        inOrder.verify(mockedMessageSender!!).sendToAll(expectedStoppedSpeaking)
+        inOrder.verify(mockedMessageSender!!).sendToAll(expectedAudioOff)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableSpeakingWithAudioDisabled() {
+        localCallParticipantModel!!.isAudioEnabled = false
+        localCallParticipantModel!!.isSpeaking = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isSpeaking = false
+
+        Mockito.verifyNoInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableVideo() {
+        localCallParticipantModel!!.isVideoEnabled = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isVideoEnabled = true
+
+        val expectedVideoOn = DataChannelMessage("videoOn")
+
+        Mockito.verify(mockedMessageSender!!).sendToAll(expectedVideoOn)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testEnableVideoTwice() {
+        localCallParticipantModel!!.isVideoEnabled = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isVideoEnabled = true
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableVideo() {
+        localCallParticipantModel!!.isVideoEnabled = true
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isVideoEnabled = false
+
+        val expectedVideoOff = DataChannelMessage("videoOff")
+
+        Mockito.verify(mockedMessageSender!!).sendToAll(expectedVideoOff)
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testDisableVideoTwice() {
+        localCallParticipantModel!!.isVideoEnabled = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localCallParticipantModel!!.isVideoEnabled = false
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+
+    @Test
+    fun testChangeStateAfterDestroying() {
+        localCallParticipantModel!!.isAudioEnabled = false
+        localCallParticipantModel!!.isSpeaking = false
+        localCallParticipantModel!!.isVideoEnabled = false
+
+        localStateBroadcaster = LocalStateBroadcaster(localCallParticipantModel, mockedMessageSender)
+
+        localStateBroadcaster!!.destroy()
+        localCallParticipantModel!!.isAudioEnabled = true
+        localCallParticipantModel!!.isSpeaking = true
+        localCallParticipantModel!!.isVideoEnabled = true
+
+        Mockito.verifyNoMoreInteractions(mockedMessageSender)
+    }
+}
