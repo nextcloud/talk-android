a78c9e1c08da3bab8a5593a0bf3505dc3588e2bf
Marcel Hibbe <dev@mhibbe.de>
2024-12-09 18:55:35 +0100

    update temp messages also for initial pull of messages
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -940,8 +940,7 @@ class ChatActivity :
         this.lifecycleScope.launch {
             chatViewModel.getRemoveMessageFlow
                 .onEach {
-                    adapter!!.delete(it)
-                    adapter!!.notifyDataSetChanged()
+                    removeMessageById(it.id)
                 }
                 .collect()
         }
@@ -1180,9 +1179,15 @@ class ChatActivity :
     }
 
     private fun removeUnreadMessagesMarker() {
-        val index = adapter?.getMessagePositionById(UNREAD_MESSAGES_MARKER_ID.toString())
+        removeMessageById(UNREAD_MESSAGES_MARKER_ID.toString())
+    }
+
+    // do not use adapter.deleteById() as it seems to contain a bug! Use this method instead!
+    private fun removeMessageById(idToDelete: String) {
+        val index = adapter?.getMessagePositionById(idToDelete)
         if (index != null && index != -1) {
             adapter?.items?.removeAt(index)
+            adapter?.notifyItemRemoved(index)
         }
     }
 
