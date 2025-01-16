4b351086a10ca46ef1b22f01d3254b9a92619f2c
Marcel Hibbe <dev@mhibbe.de>
2025-01-16 14:29:12 +0100

    use lifecycleScope from chatActivity in OutcomingTextMessageViewHolder
    defining an own scope and clearing it via onViewRecycled did not work out somehow (offline icon was wrong)
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>
    
    [skip ci]

--- app/src/main/java/com/nextcloud/talk/adapters/messages/OutcomingTextMessageViewHolder.kt
+++ app/src/main/java/com/nextcloud/talk/adapters/messages/OutcomingTextMessageViewHolder.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.lifecycleScope
 import autodagger.AutoInjector
 import coil.load
 import com.google.android.flexbox.FlexboxLayout
