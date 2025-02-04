c08a004d39397f332adaf77feb16761f5f91908a
sowjanyakch <sowjanya.kch@gmail.com>
2025-02-04 14:08:43 +0100

    format code
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/utils/message/MessageUtils.kt
+++ app/src/main/java/com/nextcloud/talk/utils/message/MessageUtils.kt
@@ -107,7 +107,7 @@ class MessageUtils(val context: Context) {
             val individualHashMap = message.messageParameters!![key]
             if (individualHashMap != null) {
                 when (individualHashMap["type"]) {
-                    "user", "guest", "call", "user-group", "email","circle" -> {
+                    "user", "guest", "call", "user-group", "email", "circle" -> {
                         val chip = if (individualHashMap["id"] == message.activeUser!!.userId) {
                             R.xml.chip_you
                         } else {
