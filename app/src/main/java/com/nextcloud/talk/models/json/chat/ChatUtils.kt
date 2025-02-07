69d38fcce7118749e41ff2276b50796d99d6029f
sowjanyakch <sowjanya.kch@gmail.com>
2025-02-07 18:20:12 +0100

    display label in the placeholder
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/models/json/chat/ChatUtils.kt
+++ app/src/main/java/com/nextcloud/talk/models/json/chat/ChatUtils.kt
@@ -39,7 +39,8 @@ class ChatUtils {
                         if (type == "file") {
                             resultMessage?.replace("{$key}", individualHashMap["name"].toString())
                         } else {
-                            individualHashMap["link"].toString()
+                            individualHashMap["name"]?.let { resultMessage?.replace("{$key}",
+                                individualHashMap["name"]!!) }
                         }
                     } else {
                         individualHashMap["name"]?.let { resultMessage?.replace("{$key}", it) }
--- app/src/main/java/com/nextcloud/talk/utils/message/MessageUtils.kt
+++ app/src/main/java/com/nextcloud/talk/utils/message/MessageUtils.kt
@@ -8,9 +8,12 @@ package com.nextcloud.talk.utils.message
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.util.Log
 import android.view.View
 import com.nextcloud.talk.R
@@ -139,6 +142,22 @@ class MessageUtils(val context: Context) {
                             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(individualHashMap["link"]))
                             context.startActivity(browserIntent)
                         }
+                    }
+                    else -> {
+                        val spannable = SpannableStringBuilder(messageStringInternal)
+                        val placeholder = "{$key}"
+                        val replacementText = individualHashMap["name"]
+                        var start = spannable.indexOf(placeholder)
+                        while (start != -1) {
+                            val end = start + placeholder.length
+                            spannable.replace(start, end, replacementText)
+                            spannable.setSpan(
+                                ForegroundColorSpan(Color.BLUE), start, start + replacementText!!
+                                .length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                            start = spannable.indexOf(placeholder, start + replacementText.length)
+                        }
+                        messageStringInternal = spannable
                     }
                 }
             }
