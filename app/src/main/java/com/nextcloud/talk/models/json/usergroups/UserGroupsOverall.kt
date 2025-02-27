287d1148a2f7cd2ee496830980bffb0e3221b939
sowjanyakch <sowjanya.kch@gmail.com>
2025-02-27 22:10:47 +0100

    add model
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

---
+++ app/src/main/java/com/nextcloud/talk/models/json/usergroups/UserGroupsOverall.kt
@@ -0,0 +1,23 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.models.json.usergroups
+
+import android.os.Parcelable
+import com.bluelinelabs.logansquare.annotation.JsonField
+import com.bluelinelabs.logansquare.annotation.JsonObject
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@JsonObject
+data class UserGroupsOverall(
+    @JsonField(name = ["ocs"])
+    var ocs: UserGroupsOCS?
+) : Parcelable {
+    // This constructor is added to work with the 'com.bluelinelabs.logansquare.annotation.JsonObject'
+    constructor() : this(null)
+}
