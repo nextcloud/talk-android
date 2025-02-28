d3051325d351c2590139ad620f6edff9e03fc853
sowjanyakch <sowjanya.kch@gmail.com>
2025-02-28 12:57:33 +0100

    fix group mentions
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -1215,9 +1215,12 @@ class ChatActivity :
 
         lifecycleScope.launch {
             chatViewModel.userGroups.collect { userGroups ->
-                chatViewModel.userCircles.collect { userCircles ->
-                    messageUtils.setUserData(userGroups, userCircles)
-                }
+                messageUtils.setUserGroups(userGroups)
+            }
+        }
+        lifecycleScope.launch {
+            chatViewModel.userCircles.collect { userCircles ->
+                messageUtils.setUserCircles(userCircles)
             }
         }
     }
