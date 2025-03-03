02838fb264e0d82b736c068b7c0b44592b496cce
Marcel Hibbe <dev@mhibbe.de>
2025-03-03 15:09:19 +0100

    migrate dropdown menu
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/diagnose/AppBar.kt
+++ app/src/main/java/com/nextcloud/talk/diagnose/AppBar.kt
@@ -8,22 +8,35 @@
 package com.nextcloud.talk.diagnose
 
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.nextcloud.talk.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar(title: String) {
+fun AppBar(title: String, menuItems: List<Pair<String, () -> Unit>>?) {
     val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+
+    var expanded by remember { mutableStateOf(false) }
 
     TopAppBar(
         title = { Text(text = title) },
@@ -36,6 +49,29 @@ fun AppBar(title: String) {
                     contentDescription = stringResource(R.string.back_button)
                 )
             }
+        },
+        actions = {
+            Box {
+                IconButton(onClick = { expanded = true }) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "More Options")
+                }
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    Modifier.background(colorResource(id = R.color.bg_default))
+                ) {
+                    menuItems?.forEach { (label, action) ->
+                        DropdownMenuItem(
+                            text = { Text(label) },
+                            onClick = {
+                                action()
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
         }
     )
 }
@@ -43,5 +79,5 @@ fun AppBar(title: String) {
 @Preview(showBackground = true)
 @Composable
 fun AppBarPreview() {
-    AppBar("title")
+    AppBar("title", null)
 }
