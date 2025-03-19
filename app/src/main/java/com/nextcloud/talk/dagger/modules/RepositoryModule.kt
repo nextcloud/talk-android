0a107c455f3c2f5bb9ef2830f0225204e7497b27
sowjanyakch <sowjanya.kch@gmail.com>
2025-03-19 10:52:51 +0100

    apply theming to user mentions
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/dagger/modules/RepositoryModule.kt
+++ app/src/main/java/com/nextcloud/talk/dagger/modules/RepositoryModule.kt
@@ -193,8 +193,8 @@ class RepositoryModule {
     fun provideUserGroupsCirclesRepository(
         userCirclesOrGroupsDao: UserCirclesOrGroupsDao,
         ncApiCoroutines: NcApiCoroutines,
-        userProvider: CurrentUserProviderNew
+        currentUserProvider: CurrentUserProviderNew
     ): UserGroupsCirclesRepository {
-        return UserGroupsCirclesRepository(userCirclesOrGroupsDao, ncApiCoroutines, userProvider)
+        return UserGroupsCirclesRepository(userCirclesOrGroupsDao, ncApiCoroutines, currentUserProvider)
     }
 }
