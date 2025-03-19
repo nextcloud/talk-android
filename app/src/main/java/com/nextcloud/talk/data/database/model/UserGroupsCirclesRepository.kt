a5cf64e22955104d4041bf641892ccb9172f1514
sowjanyakch <sowjanya.kch@gmail.com>
2025-03-19 11:00:55 +0100

    ktlintformat
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/data/database/model/UserGroupsCirclesRepository.kt
+++ app/src/main/java/com/nextcloud/talk/data/database/model/UserGroupsCirclesRepository.kt
@@ -24,58 +24,60 @@ class UserGroupsCirclesRepository @Inject constructor(
     private val currentUserProvider: CurrentUserProviderNew
 ) {
     val user = currentUserProvider.currentUser.blockingGet()
-    suspend fun initialize(): Boolean = withContext(Dispatchers.IO) {
-        try {
-            val credentials: String = ApiUtils.getCredentials(user.username, user.token)!!
 
-            coroutineScope {
-                launch {
-                   userCirclesOrGroupsDao.deleteAllUserGroups()
-                    val response = ncApiCoroutines.getUserGroups(
-                        credentials,
-                        ApiUtils.getUrlForUserGroups(
-                            user.baseUrl!!,
-                            user.userId!!
+    @Suppress("Detekt.TooGenericExceptionCaught")
+    suspend fun initialize(): Boolean =
+        withContext(Dispatchers.IO) {
+            try {
+                val credentials: String = ApiUtils.getCredentials(user.username, user.token)!!
+
+                coroutineScope {
+                    launch {
+                        userCirclesOrGroupsDao.deleteAllUserGroups()
+                        val response = ncApiCoroutines.getUserGroups(
+                            credentials,
+                            ApiUtils.getUrlForUserGroups(
+                                user.baseUrl!!,
+                                user.userId!!
+                            )
                         )
-                    )
-                    val groups = response.ocs?.data?.groups?: emptyList()
-                    Log.d("UserDataRepo","$groups")
-                    userCirclesOrGroupsDao.insertUserGroups(
-                        groups.map{
-                            UserGroupsEntity(it)
-                        }
-                    )
+                        val groups = response.ocs?.data?.groups ?: emptyList()
+                        Log.d("UserDataRepo", "$groups")
+                        userCirclesOrGroupsDao.insertUserGroups(
+                            groups.map {
+                                UserGroupsEntity(it)
+                            }
+                        )
+                    }
+
+                    launch {
+                        userCirclesOrGroupsDao.deleteAllUserCircles()
+                        val response = ncApiCoroutines.getUserCircles(
+                            credentials,
+                            ApiUtils.getUrlForUserCircles(user.baseUrl!!)
+                        )
+                        val circles = response.ocs?.data?.map { it.displayName!! } ?: emptyList()
+                        Log.d("UserDataRepo", "$circles")
+                        userCirclesOrGroupsDao.insertUserCircles(
+                            circles.map {
+                                UserCirclesEntity(it)
+                            }
+                        )
+                    }
                 }
 
-                launch {
-                    userCirclesOrGroupsDao.deleteAllUserCircles()
-                    val response = ncApiCoroutines.getUserCircles(
-                        credentials,
-                        ApiUtils.getUrlForUserCircles(user.baseUrl!!)
-                    )
-                    val circles = response.ocs?.data?.map { it.displayName!! }?: emptyList()
-                    Log.d("UserDataRepo","$circles")
-                    userCirclesOrGroupsDao.insertUserCircles(
-                        circles.map{
-                            UserCirclesEntity(it)
-                        }
-                    )
-                }
+                return@withContext true
+            } catch (e: Exception) {
+                Log.e("UserDataRepo", "Error initializing user data", e)
+                return@withContext false
             }
-
-            return@withContext true
-        } catch (e: Exception) {
-            Log.e("UserDataRepo", "Error initializing user data", e)
-            return@withContext false
         }
 
-    }
-
-   fun getUserGroups(): List<UserGroupsEntity> {
+    fun getUserGroups(): List<UserGroupsEntity> {
         return userCirclesOrGroupsDao.getUserGroups()
     }
 
-   fun getUserCircles(): List<UserCirclesEntity>{
+    fun getUserCircles(): List<UserCirclesEntity> {
         return userCirclesOrGroupsDao.getUserCircles()
     }
 }
