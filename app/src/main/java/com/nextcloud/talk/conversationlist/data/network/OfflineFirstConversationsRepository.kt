55883341918230a084c48fb71d77bc90b7b7cee3
sowjanyakch <sowjanya.kch@gmail.com>
2025-04-08 17:14:30 +0200

    UI changes and hiding events
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/conversationlist/data/network/OfflineFirstConversationsRepository.kt
+++ app/src/main/java/com/nextcloud/talk/conversationlist/data/network/OfflineFirstConversationsRepository.kt
@@ -18,6 +18,7 @@ import com.nextcloud.talk.data.database.model.ConversationEntity
 import com.nextcloud.talk.data.network.NetworkMonitor
 import com.nextcloud.talk.data.user.model.User
 import com.nextcloud.talk.models.domain.ConversationModel
+import com.nextcloud.talk.models.json.conversations.ConversationEnums
 import com.nextcloud.talk.utils.CapabilitiesUtil.isUserStatusAvailable
 import com.nextcloud.talk.utils.database.user.CurrentUserProviderNew
 import io.reactivex.Observer
@@ -121,7 +122,15 @@ class OfflineFirstConversationsRepository @Inject constructor(
                 .observeOn(AndroidSchedulers.mainThread())
                 .blockingSingle()
 
-            conversationsFromSync = conversationsList.map {
+            val currentTime = System.currentTimeMillis() / 1000
+
+            val conversationListWithoutEvents = conversationsList.filterNot { conversation ->
+
+                conversation.objectType == ConversationEnums.ObjectType.EVENT &&
+                    conversation.objectId.split("#")[0].toLong() - currentTime >
+                    AGE_THRESHOLD_FOR_EVENT_CONVERSATIONS
+            }
+            conversationsFromSync = conversationListWithoutEvents.map {
                 it.asEntity(user.id!!)
             }
 
@@ -156,5 +165,6 @@ class OfflineFirstConversationsRepository @Inject constructor(
 
     companion object {
         val TAG = OfflineFirstConversationsRepository::class.simpleName
+        private const val AGE_THRESHOLD_FOR_EVENT_CONVERSATIONS: Long = 86400
     }
 }
