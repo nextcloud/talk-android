7fa260da90aeb278928a47e0491558e477674e26
Marcel Hibbe <dev@mhibbe.de>
2025-06-02 20:08:17 +0200

    avoid duplicate messages by introducing a sendStatus
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

---
+++ app/src/main/java/com/nextcloud/talk/data/database/model/SendStatus.kt
@@ -0,0 +1,14 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.data.database.model
+
+enum class SendStatus {
+    PENDING,
+    SENT_PENDING_ACK,
+    FAILED
+}
