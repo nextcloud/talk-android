177fa1ea84ae0a028e454435327ba611e653fb24
Marcel Hibbe <dev@mhibbe.de>
2025-06-13 11:20:36 +0200

    Make navigationBar fully transparent for Android 15
    = Change navigationBarStyle to SystemBarStyle.light
    
    This is done to align with the XML screens, see comment.
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/utils/AppCompatActivityExtensions.kt
+++ app/src/main/java/com/nextcloud/talk/utils/AppCompatActivityExtensions.kt
@@ -21,7 +21,14 @@ import androidx.appcompat.app.AppCompatActivity
 @JvmOverloads
 fun AppCompatActivity.adjustUIForAPILevel35(
     statusBarStyle: SystemBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
-    navigationBarStyle: SystemBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+    // It may make sense to change navigationBarStyle to "SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)"
+    // For now, it is set to "light" to have a fully transparent navigation bar to align with the XML screens.
+    // It may be wanted to have a semi transparent navigation bar in the future. Then set it to "auto" and try to
+    // migrate the XML screens to Compose (having semi transparent navigation bar for XML did not work out. In
+    // general, supporting both XML and Compose system bar handling is a pain and we will have it easier without XML)
+    // So in short: migrate all screens to Compose. Then it's easier to decide if navigation bar should be semi
+    // transparent or not for all screens.
+    navigationBarStyle: SystemBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
 ) {
     val isApiLevel35OrHigher = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM)
     if (!isApiLevel35OrHigher) {
