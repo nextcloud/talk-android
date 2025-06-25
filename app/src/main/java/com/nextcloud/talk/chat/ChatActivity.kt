c112ce45d2957abe3a8c0fc6e080f67e6bdd6218
sowjanyakch <sowjanya.kch@gmail.com>
2025-06-25 18:18:35 +0200

    format
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -1334,7 +1334,7 @@ class ChatActivity :
 
         cancelNotificationsForCurrentConversation()
 
-        if(adapter == null){
+        if (adapter == null) {
             chatViewModel.getRoom(roomToken)
         }
 
