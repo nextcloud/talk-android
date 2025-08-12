a98648216980f62506648980db153d308e4f2110
sowjanyakch <sowjanya.kch@gmail.com>
2025-08-12 18:29:24 +0200

    using composable with MVVM and coroutines
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/api/NcApiCoroutines.kt
+++ app/src/main/java/com/nextcloud/talk/api/NcApiCoroutines.kt
@@ -317,10 +317,7 @@ interface NcApiCoroutines {
     ): RoomsOverall
 
     @GET
-    suspend fun getInvitations(
-        @Header("Authorization") authorization: String,
-        @Url url: String
-    ): InvitationOverall
+    suspend fun getInvitations(@Header("Authorization") authorization: String, @Url url: String): InvitationOverall
 
     @GET
     suspend fun status(@Header("Authorization") authorization: String, @Url url: String): StatusOverall
--- app/src/main/java/com/nextcloud/talk/conversationlist/ConversationsListActivity.kt
+++ app/src/main/java/com/nextcloud/talk/conversationlist/ConversationsListActivity.kt
@@ -112,7 +112,7 @@ import com.nextcloud.talk.repositories.unifiedsearch.UnifiedSearchRepository
 import com.nextcloud.talk.settings.SettingsActivity
 import com.nextcloud.talk.threadsoverview.ThreadsOverviewActivity
 import com.nextcloud.talk.ui.BackgroundVoiceMessageCard
-import com.nextcloud.talk.ui.dialog.ChooseAccountDialogFragment
+import com.nextcloud.talk.ui.dialog.ChooseAccountDialogCompose
 import com.nextcloud.talk.ui.dialog.ChooseAccountShareToDialogFragment
 import com.nextcloud.talk.ui.dialog.ConversationsListBottomDialog
 import com.nextcloud.talk.ui.dialog.FilterConversationFragment
@@ -825,6 +825,15 @@ class ConversationsListActivity :
         }
     }
 
+    private fun showChooseAccountDialog() {
+        binding.genericComposeView.apply {
+            val shouldDismiss = mutableStateOf(false)
+            setContent {
+                ChooseAccountDialogCompose().GetChooseAccountDialog(shouldDismiss, this@ConversationsListActivity)
+            }
+        }
+    }
+
     private fun loadUserAvatar(button: MaterialButton) {
         val target = object : Target {
             override fun onStart(placeholder: Drawable?) {
@@ -1176,8 +1185,7 @@ class ConversationsListActivity :
 
             if (resources!!.getBoolean(R.bool.multiaccount_support) && userManager.users.blockingGet().size > 1) {
                 dialogBuilder.setPositiveButton(R.string.nc_switch_account) { _, _ ->
-                    val newFragment: DialogFragment = ChooseAccountDialogFragment.newInstance()
-                    newFragment.show(supportFragmentManager, ChooseAccountDialogFragment.TAG)
+                    showChooseAccountDialog()
                 }
             }
 
@@ -1296,8 +1304,7 @@ class ConversationsListActivity :
 
         binding.switchAccountButton.setOnClickListener {
             if (resources != null && resources!!.getBoolean(R.bool.multiaccount_support)) {
-                val newFragment: DialogFragment = ChooseAccountDialogFragment.newInstance()
-                newFragment.show(supportFragmentManager, ChooseAccountDialogFragment.TAG)
+                showChooseAccountDialog()
             } else {
                 val intent = Intent(context, SettingsActivity::class.java)
                 startActivity(intent)
@@ -2069,8 +2076,7 @@ class ConversationsListActivity :
 
             if (resources!!.getBoolean(R.bool.multiaccount_support) && userManager.users.blockingGet().size > 1) {
                 dialogBuilder.setNegativeButton(R.string.nc_switch_account) { _, _ ->
-                    val newFragment: DialogFragment = ChooseAccountDialogFragment.newInstance()
-                    newFragment.show(supportFragmentManager, ChooseAccountDialogFragment.TAG)
+                    showChooseAccountDialog()
                 }
             }
 
@@ -2113,8 +2119,7 @@ class ConversationsListActivity :
 
             if (resources!!.getBoolean(R.bool.multiaccount_support) && userManager.users.blockingGet().size > 1) {
                 dialogBuilder.setNegativeButton(R.string.nc_switch_account) { _, _ ->
-                    val newFragment: DialogFragment = ChooseAccountDialogFragment.newInstance()
-                    newFragment.show(supportFragmentManager, ChooseAccountDialogFragment.TAG)
+                    showChooseAccountDialog()
                 }
             }
 
