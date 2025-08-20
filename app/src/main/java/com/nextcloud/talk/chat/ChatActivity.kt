674d8aa72d5bdd3b2e7af505063f7ceb2055c26d
sowjanyakch <sowjanya.kch@gmail.com>
2025-08-20 19:09:28 +0200

    don't group system messages over date separator
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -3492,24 +3492,29 @@ class ChatActivity :
     private fun handleExpandableSystemMessages(chatMessageList: List<ChatMessage>): List<ChatMessage> {
         val chatMessageMap = chatMessageList.associateBy { it.id }.toMutableMap()
         val chatMessageIterator = chatMessageMap.iterator()
+
         while (chatMessageIterator.hasNext()) {
-            val currentMessage = chatMessageIterator.next()
+            val currentMessage = chatMessageIterator.next().value
 
-            val previousMessage = chatMessageMap[currentMessage.value.previousMessageId.toString()]
-            if (isSystemMessage(currentMessage.value) &&
-                previousMessage?.systemMessageType == currentMessage.value.systemMessageType
-            ) {
-                previousMessage?.expandableParent = true
-                currentMessage.value.expandableParent = false
+            chatMessageMap[currentMessage.previousMessageId.toString()]?.let { previousMessage ->
+                if (
+                    isSystemMessage(currentMessage) &&
+                    previousMessage.systemMessageType == currentMessage.systemMessageType &&
+                    isSameDayMessages(previousMessage, currentMessage)
+                ) {
+                    previousMessage.expandableParent = true
+                    currentMessage.expandableParent = false
 
-                if (currentMessage.value.lastItemOfExpandableGroup == 0) {
-                    currentMessage.value.lastItemOfExpandableGroup = currentMessage.value.jsonMessageId
+                    if (currentMessage.lastItemOfExpandableGroup == 0) {
+                        currentMessage.lastItemOfExpandableGroup = currentMessage.jsonMessageId
+                    }
+
+                    previousMessage.lastItemOfExpandableGroup = currentMessage.lastItemOfExpandableGroup
+                    previousMessage.expandableChildrenAmount = currentMessage.expandableChildrenAmount + 1
                 }
-
-                previousMessage?.lastItemOfExpandableGroup = currentMessage.value.lastItemOfExpandableGroup
-                previousMessage?.expandableChildrenAmount = currentMessage.value.expandableChildrenAmount + 1
             }
         }
+
         return chatMessageMap.values.toList()
     }
 
