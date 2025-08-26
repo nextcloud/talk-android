017ba459a2b669ea65094a845d2a624e242525c4
sowjanyakch <sowjanya.kch@gmail.com>
2025-08-26 19:10:21 +0200

    remove unused imports
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/conversationcreation/ConversationCreationActivity.kt
+++ app/src/main/java/com/nextcloud/talk/conversationcreation/ConversationCreationActivity.kt
@@ -37,7 +37,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
