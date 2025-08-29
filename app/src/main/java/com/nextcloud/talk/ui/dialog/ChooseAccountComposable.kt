6e690c2e9454fc5504a758839b37951904a93077
sowjanyakch <sowjanya.kch@gmail.com>
2025-08-29 22:28:51 +0200

    add header
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/ui/dialog/ChooseAccountComposable.kt
+++ app/src/main/java/com/nextcloud/talk/ui/dialog/ChooseAccountComposable.kt
@@ -1,3 +1,10 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Sowjanya Kota <sowjanya.kch@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 package com.nextcloud.talk.ui.dialog
 
 import android.app.Activity
