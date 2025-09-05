1b945c57c8de4c41f2025cbf45eac5c9aba30874
Marcel Hibbe <dev@mhibbe.de>
2025-09-05 15:21:25 +0200

    add thread icons, improve ColorGenerator and add tests
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/utils/ColorGenerator.kt
+++ app/src/main/java/com/nextcloud/talk/utils/ColorGenerator.kt
@@ -10,98 +10,60 @@ package com.nextcloud.talk.utils
 
 import android.graphics.Color
 import java.security.MessageDigest
-import kotlin.math.abs
 
 // See https://github.com/nextcloud/nextcloud-vue/blob/56b79afae93f4701a0cb933bfeb7b4a2fbd590fb/src/functions/usernameToColor/usernameToColor.js
 // and https://github.com/nextcloud/nextcloud-vue/blob/56b79afae93f4701a0cb933bfeb7b4a2fbd590fb/src/utils/GenColors.js
 
 @Suppress("MagicNumber")
-class ColorGenerator private constructor() {
+object ColorGenerator {
 
-    private val steps = 6
-    private val finalPalette: List<Int> = genColors(steps)
+    private const val STEPS = 6
+    private val finalPalette: List<RGB> = genColors()
 
-    companion object {
-        val shared = ColorGenerator()
-        private const val MULTIPLIER = 256.0f
-
-        private fun stepCalc(steps: Int, colorStart: Int, colorEnd: Int): List<Float> {
-            val r0 = Color.red(colorStart) / MULTIPLIER
-            val g0 = Color.green(colorStart) / MULTIPLIER
-            val b0 = Color.blue(colorStart) / MULTIPLIER
-
-            val r1 = Color.red(colorEnd) / MULTIPLIER
-            val g1 = Color.green(colorEnd) / MULTIPLIER
-            val b1 = Color.blue(colorEnd) / MULTIPLIER
-
-            return listOf(
-                (r1 - r0) / steps,
-                (g1 - g0) / steps,
-                (b1 - b0) / steps
-            )
-        }
-
-        private fun mixPalette(steps: Int, color1: Int, color2: Int): List<Int> {
-            val palette = mutableListOf(color1)
-            val step = stepCalc(steps, color1, color2)
-
-            val rStart = Color.red(color1) / MULTIPLIER
-            val gStart = Color.green(color1) / MULTIPLIER
-            val bStart = Color.blue(color1) / MULTIPLIER
-
-            for (i in 1 until steps) {
-                val r = (abs(rStart + step[0] * i) * MULTIPLIER).toInt().coerceIn(0, 255)
-                val g = (abs(gStart + step[1] * i) * MULTIPLIER).toInt().coerceIn(0, 255)
-                val b = (abs(bStart + step[2] * i) * MULTIPLIER).toInt().coerceIn(0, 255)
-
-                palette.add(Color.rgb(r, g, b))
-            }
-
-            return palette
-        }
-
-        fun genColors(steps: Int): List<Int> {
-            val red = Color.rgb(
-                (182 / MULTIPLIER * 255).toInt(),
-                (70 / MULTIPLIER * 255).toInt(),
-                (157 / MULTIPLIER * 255).toInt()
-            )
-            val yellow = Color.rgb(
-                (221 / MULTIPLIER * 255).toInt(),
-                (203 / MULTIPLIER * 255).toInt(),
-                (85 / MULTIPLIER * 255).toInt()
-            )
-            val blue = Color.rgb(
-                0,
-                (130 / MULTIPLIER * 255).toInt(),
-                (201 / MULTIPLIER * 255).toInt()
-            )
-
-            val palette1 = mixPalette(steps, red, yellow).toMutableList()
-            val palette2 = mixPalette(steps, yellow, blue)
-            val palette3 = mixPalette(steps, blue, red)
-
-            palette1.addAll(palette2)
-            palette1.addAll(palette3)
-
-            return palette1
-        }
-    }
+    private data class RGB(val r: Int, val g: Int, val b: Int)
 
     fun usernameToColor(username: String): Int {
-        val hash = username.lowercase()
-        var hashInt: Int
+        var hash = username.lowercase()
 
-        val bytes = hash.toByteArray(Charsets.UTF_8)
-        val md = MessageDigest.getInstance("MD5")
-        val digest = md.digest(bytes)
-        val hex = digest.joinToString("") { "%02x".format(it) }
+        val md5Regex = Regex("^([0-9a-f]{4}-?){8}$")
+        if (!hash.matches(md5Regex)) {
+            val digest = MessageDigest.getInstance("MD5").digest(hash.toByteArray(Charsets.UTF_8))
+            hash = digest.joinToString("") { "%02x".format(it) }
+        }
 
-        hashInt = hex.map { it.digitToInt(16) % 16 }.sum()
+        hash = hash.replace(Regex("[^0-9a-f]"), "")
 
-        val maximum = steps * 3
-        hashInt %= maximum
+        val idx = hashToInt(hash, finalPalette.size)
 
-        return finalPalette[hashInt]
+        val rgb = finalPalette[idx]
+        return Color.rgb(rgb.r, rgb.g, rgb.b)
+    }
+
+    private fun hashToInt(hash: String, maximum: Int): Int {
+        val sum = hash.map { it.lowercaseChar().digitToInt(16) % 16 }.sum()
+        return sum % maximum
+    }
+
+    private fun genColors(): List<RGB> {
+        val red = RGB(182, 70, 157)
+        val yellow = RGB(221, 203, 85)
+        val blue = RGB(0, 130, 201)
+
+        return mixPalette(red, yellow) + mixPalette(yellow, blue) + mixPalette(blue, red)
+    }
+
+    private fun mixPalette(start: RGB, end: RGB): List<RGB> {
+        val palette = mutableListOf(start)
+        val rStep = (end.r - start.r).toFloat() / STEPS
+        val gStep = (end.g - start.g).toFloat() / STEPS
+        val bStep = (end.b - start.b).toFloat() / STEPS
+
+        for (i in 1 until STEPS) {
+            val r = (start.r + rStep * i).toInt().coerceIn(0, 255)
+            val g = (start.g + gStep * i).toInt().coerceIn(0, 255)
+            val b = (start.b + bStep * i).toInt().coerceIn(0, 255)
+            palette.add(RGB(r, g, b))
+        }
+        return palette
     }
 }
