878b45d702610a4d405b3e4b86306fb20c6cd14f
Marcel Hibbe <dev@mhibbe.de>
2025-09-09 13:59:50 +0200

    remove deprecated code for thread creation
    thread is now created while posting a message (concept and API changed)
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/chat/viewmodels/ChatViewModel.kt
+++ app/src/main/java/com/nextcloud/talk/chat/viewmodels/ChatViewModel.kt
@@ -169,9 +169,6 @@ class ChatViewModel @Inject constructor(
     val getContextChatMessages: LiveData<List<ChatMessageJson>>
         get() = _getContextChatMessages
 
-    private val _threadCreationState = MutableStateFlow<ThreadCreationUiState>(ThreadCreationUiState.None)
-    val threadCreationState: StateFlow<ThreadCreationUiState> = _threadCreationState
-
     private val _threadRetrieveState = MutableStateFlow<ThreadRetrieveUiState>(ThreadRetrieveUiState.None)
     val threadRetrieveState: StateFlow<ThreadRetrieveUiState> = _threadRetrieveState
 
@@ -448,18 +445,6 @@ class ChatViewModel @Inject constructor(
                     _createRoomViewState.value = CreateRoomSuccessState(t)
                 }
             })
-    }
-
-    @Suppress("Detekt.TooGenericExceptionCaught")
-    fun createThread(credentials: String, url: String) {
-        viewModelScope.launch {
-            try {
-                val thread = chatNetworkDataSource.createThread(credentials, url)
-                _threadCreationState.value = ThreadCreationUiState.Success(thread.ocs?.data)
-            } catch (exception: Exception) {
-                _threadCreationState.value = ThreadCreationUiState.Error(exception)
-            }
-        }
     }
 
     @Suppress("Detekt.TooGenericExceptionCaught")
@@ -984,12 +969,6 @@ class ChatViewModel @Inject constructor(
         data object None : UnbindRoomUiState()
         data class Success(val statusCode: Int) : UnbindRoomUiState()
         data class Error(val message: String) : UnbindRoomUiState()
-    }
-
-    sealed class ThreadCreationUiState {
-        data object None : ThreadCreationUiState()
-        data class Success(val thread: ThreadInfo?) : ThreadCreationUiState()
-        data class Error(val exception: Exception) : ThreadCreationUiState()
     }
 
     sealed class ThreadRetrieveUiState {
