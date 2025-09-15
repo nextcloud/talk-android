b1b96616c984596ff3d0c0f0c9eb2539ee524397
sowjanyakch <sowjanya.kch@gmail.com>
2025-09-15 18:09:57 +0200

    remove unused code
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/adapters/messages/IncomingTextMessageViewHolder.kt
+++ app/src/main/java/com/nextcloud/talk/adapters/messages/IncomingTextMessageViewHolder.kt
@@ -443,8 +443,6 @@ class IncomingTextMessageViewHolder(itemView: View, payload: Any) :
     companion object {
         const val TEXT_SIZE_MULTIPLIER = 2.5
         private val TAG = IncomingTextMessageViewHolder::class.java.simpleName
-        private const val CHECKED_GROUP_INDEX = 2
-        private const val TASK_TEXT_GROUP_INDEX = 3
         private const val AGE_THRESHOLD_FOR_EDIT_MESSAGE: Long = 86400000
     }
 }
