12a9c8553a61f7c854e5ff9b95c68e9ffc6d4747
Marcel Hibbe <dev@mhibbe.de>
2025-09-18 11:56:09 +0200

    add missing SPDX-FileCopyrightTexts
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/account/WebViewLoginActivity.kt
+++ app/src/main/java/com/nextcloud/talk/account/WebViewLoginActivity.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud Talk - Android Client
  *
- * SPDX-FileCopyrightText: 2025 Your Name <your@email.com>
+ * SPDX-FileCopyrightText: 2025 Julius Linus <juliuslinus1@gmail.com>
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 package com.nextcloud.talk.account
