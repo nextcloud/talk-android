92ba0a981a949b62219457f37f18492c9e0c79cd
sowjanyakch <sowjanya.kch@gmail.com>
2025-09-18 21:50:11 +0200

    create data class - AccountItem
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

---
+++ app/src/main/java/com/nextcloud/talk/account/data/model/AccountItem.kt
@@ -0,0 +1,12 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.account.data.model
+
+import com.nextcloud.talk.data.user.model.User
+
+data class AccountItem(val user: User, val userId: String?)
