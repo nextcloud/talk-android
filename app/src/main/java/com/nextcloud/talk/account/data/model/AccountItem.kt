65e78d8f8e1e0cd9fb1e9f756ed2b806edbb070b
sowjanyakch <sowjanya.kch@gmail.com>
2025-09-18 21:53:31 +0200

    add license header info
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/account/data/model/AccountItem.kt
+++ app/src/main/java/com/nextcloud/talk/account/data/model/AccountItem.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud Talk - Android Client
  *
- * SPDX-FileCopyrightText: 2025 Your Name <your@email.com>
+ * SPDX-FileCopyrightText: 2025 Sowjanya Kota <sowjanya.kch@gmail.com>
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
