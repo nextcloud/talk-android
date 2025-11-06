55210f04ad04707d848b2b9205d9dcf7c85fcae7
Marcel Hibbe <dev@mhibbe.de>
2025-11-06 13:54:49 +0100

    fix federated avatar darkMode + make avatar handling even more reactive
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/call/components/AvatarWithFallback.kt
+++ app/src/main/java/com/nextcloud/talk/call/components/AvatarWithFallback.kt
@@ -18,26 +18,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.nextcloud.talk.activities.ParticipantUiState
+import com.nextcloud.talk.models.json.participants.Participant
+import com.nextcloud.talk.utils.ApiUtils
+import com.nextcloud.talk.utils.DisplayUtils.isDarkModeOn
 
 @Composable
 fun AvatarWithFallback(participant: ParticipantUiState, modifier: Modifier = Modifier) {
-    val initials = participant.nick!!
-        .split(" ")
-        .mapNotNull { it.firstOrNull()?.uppercase() }
-        .take(2)
-        .joinToString("")
-
     Box(
         modifier = modifier
             .clip(CircleShape),
         contentAlignment = Alignment.Center
     ) {
-        if (!participant.avatarUrl.isNullOrEmpty()) {
+        val avatarUrl = getUrlForAvatar(participant = participant)
+        if (avatarUrl.isNotEmpty()) {
             AsyncImage(
-                model = participant.avatarUrl,
+                model = getUrlForAvatar(participant = participant),
                 contentDescription = "Avatar",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -45,18 +44,53 @@ fun AvatarWithFallback(participant: ParticipantUiState, modifier: Modifier = Mod
                     .clip(CircleShape)
             )
         } else {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White, CircleShape),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = initials.ifEmpty { "?" },
-                    color = Color.Black,
-                    fontSize = 24.sp
-                )
-            }
+            FallbackAvatar(participant = participant)
         }
     }
+}
+
+@Composable
+private fun FallbackAvatar(participant: ParticipantUiState) {
+    val initials = participant.nick!!
+        .split(" ")
+        .mapNotNull { it.firstOrNull()?.uppercase() }
+        .take(2)
+        .joinToString("")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White, CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = initials.ifEmpty { "?" },
+            color = Color.Black,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Composable
+fun getUrlForAvatar(participant: ParticipantUiState): String {
+    var url = ApiUtils.getUrlForAvatar(participant.baseUrl, participant.actorId, false)
+    if (Participant.ActorType.GUESTS == participant.actorType ||
+        Participant.ActorType.EMAILS == participant.actorType
+    ) {
+        url = ApiUtils.getUrlForGuestAvatar(
+            participant.baseUrl,
+            participant.nick,
+            true
+        )
+    }
+    if (participant.actorType == Participant.ActorType.FEDERATED) {
+        val darkTheme = if (isDarkModeOn(LocalContext.current)) 1 else 0
+        url = ApiUtils.getUrlForFederatedAvatar(
+            participant.baseUrl,
+            participant.roomToken,
+            participant.actorId!!,
+            darkTheme,
+            false
+        )
+    }
+    return url
 }
