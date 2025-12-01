7893c83f466e8029fea208b42c8b7c8598e6d393
Marcel Hibbe <dev@mhibbe.de>
2025-12-01 13:51:15 +0100

    rewrite CurrentUserProviderImpl with coroutines
    usage not applied yet..
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/utils/database/user/CurrentUserProviderImpl.kt
+++ app/src/main/java/com/nextcloud/talk/utils/database/user/CurrentUserProviderImpl.kt
@@ -9,36 +9,38 @@ package com.nextcloud.talk.utils.database.user
 
 import com.nextcloud.talk.data.user.model.User
 import com.nextcloud.talk.users.UserManager
-import io.reactivex.Maybe
-import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.rx2.asFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
-/**
- * Listens to changes in the database and provides the current user without needing to query the database everytime.
- */
 @Singleton
 class CurrentUserProviderImpl @Inject constructor(private val userManager: UserManager) : CurrentUserProviderNew {
 
-    private var _currentUser: User? = null
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
-    // synchronized to avoid multiple observers initialized from different threads
-    @get:Synchronized
-    @set:Synchronized
-    private var currentUserObserver: Disposable? = null
+    private val _currentUser: StateFlow<User?> = userManager.currentUserObservable
+        .asFlow()
+        .stateIn(
+            scope = scope,
+            started = SharingStarted.Eagerly,
+            initialValue = null
+        )
 
-    override val currentUser: Maybe<User>
-        get() {
-            if (_currentUser == null) {
-                // immediately get a result synchronously
-                _currentUser = userManager.currentUser.blockingGet()
-                if (currentUserObserver == null) {
-                    currentUserObserver = userManager.currentUserObservable
-                        .subscribe {
-                            _currentUser = it
-                        }
-                }
-            }
-            return _currentUser?.let { Maybe.just(it) } ?: Maybe.empty()
-        }
+    // only emit non-null users
+    val currentUserFlow: Flow<User> = _currentUser.filterNotNull()
+
+    // function for safe one-shot access
+    override suspend fun getCurrentUser(): User {
+        // suspends until a non-null user is available
+        return currentUserFlow.first()
+    }
 }
