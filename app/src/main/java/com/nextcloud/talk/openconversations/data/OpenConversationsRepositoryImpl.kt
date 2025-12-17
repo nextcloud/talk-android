023588bf9c347fda2566fffc0873931ad82ea03f
rapterjet2004 <juliuslinus1@gmail.com>
2025-12-17 11:20:59 -0600

    - redoing the querying logic to use combine converting message search functionality to a flow based approach - moving the activity logic to my viewModel, and observing the result properly - this adds the conversations to the combine flow, which simplifies the activity and filtering logic, resolving race conditions
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

--- app/src/main/java/com/nextcloud/talk/openconversations/data/OpenConversationsRepositoryImpl.kt
+++ app/src/main/java/com/nextcloud/talk/openconversations/data/OpenConversationsRepositoryImpl.kt
@@ -10,6 +10,8 @@ import com.nextcloud.talk.api.NcApiCoroutines
 import com.nextcloud.talk.data.user.model.User
 import com.nextcloud.talk.models.json.conversations.Conversation
 import com.nextcloud.talk.utils.ApiUtils
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class OpenConversationsRepositoryImpl(private val ncApiCoroutines: NcApiCoroutines) : OpenConversationsRepository {
     override suspend fun fetchConversations(user: User, url: String, searchTerm: String): Result<List<Conversation>> =
@@ -22,5 +24,28 @@ class OpenConversationsRepositoryImpl(private val ncApiCoroutines: NcApiCoroutin
                 searchTerm
             )
             roomOverall.ocs?.data.orEmpty()
+        }
+
+    override fun fetchOpenConversationsFlow(user: User, searchTerm: String): Flow<List<Conversation>> =
+        flow {
+            val credentials: String = ApiUtils.getCredentials(user.username, user.token)!!
+
+            val apiVersion = ApiUtils.getConversationApiVersion(
+                user,
+                intArrayOf(
+                    ApiUtils.API_V4,
+                    ApiUtils.API_V3,
+                    1
+                )
+            )
+            val url = ApiUtils.getUrlForOpenConversations(apiVersion, user.baseUrl!!)
+
+            val roomOverall = ncApiCoroutines.getOpenConversations(
+                credentials,
+                url,
+                searchTerm
+            )
+
+            emit(roomOverall.ocs?.data.orEmpty())
         }
 }
