a2b6780cbe8933442f1a3a19e401a4d157bfca5d
rapterjet2004 <juliuslinus1@gmail.com>
2025-12-19 09:50:25 -0600

    linter
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/MessageInputFragment.kt
+++ app/src/main/java/com/nextcloud/talk/chat/MessageInputFragment.kt
@@ -173,12 +173,15 @@ class MessageInputFragment : Fragment() {
     // https://stackoverflow.com/a/54648758/14183836
     // Would be easier to move the capabilities observability to kotlin flows/suspend
     fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
-        observe(lifecycleOwner, object : Observer<T> {
-            override fun onChanged(value: T) {
-                observer.onChanged(value)
-                removeObserver(this)
+        observe(
+            lifecycleOwner,
+            object : Observer<T> {
+                override fun onChanged(value: T) {
+                    observer.onChanged(value)
+                    removeObserver(this)
+                }
             }
-        })
+        )
     }
 
     private fun initObservers() {
