a86f9caf393db82ed08581efacf07671b7ae48d0
sowjanyakch <sowjanya.kch@gmail.com>
2026-01-06 15:24:03 +0100

    edit captions
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/ui/dialog/MessageActionsDialog.kt
+++ app/src/main/java/com/nextcloud/talk/ui/dialog/MessageActionsDialog.kt
@@ -113,11 +113,13 @@ class MessageActionsDialog(
             ) &&
         !isOlderThanTwentyFourHours
 
+    private val messageHasCaptions = messageHasFileAttachment && message.message!= "{file}" && !message.isDeleted
+
     private var messageIsEditable = hasSpreedFeatureCapability(
         spreedCapabilities,
         SpreedFeatures.EDIT_MESSAGES
     ) &&
-        messageHasRegularText &&
+        (messageHasRegularText || messageHasCaptions) &&
         !isOlderThanTwentyFourHours &&
         isUserAllowedToEdit
 
