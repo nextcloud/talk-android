ed8e1b1c084d40cf74b0ee8847eebbe5ab4e51b5
sowjanyakch <sowjanya.kch@gmail.com>
2026-01-06 16:43:09 +0100

    remove extra spacing + format
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/ui/dialog/MessageActionsDialog.kt
+++ app/src/main/java/com/nextcloud/talk/ui/dialog/MessageActionsDialog.kt
@@ -113,7 +113,7 @@ class MessageActionsDialog(
             ) &&
         !isOlderThanTwentyFourHours
 
-    private val messageHasCaptions = messageHasFileAttachment && message.message!= "{file}" && !message.isDeleted
+    private val messageHasCaptions = messageHasFileAttachment && message.message != "{file}" && !message.isDeleted
 
     private var messageIsEditable = hasSpreedFeatureCapability(
         spreedCapabilities,
