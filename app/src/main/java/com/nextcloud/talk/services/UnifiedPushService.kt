b2c702f2bd4be781b209eb55d75cdf5dfe340b4c
sim <git@sgougeon.fr>
2026-01-15 15:35:01 +0100

    Add UnifiedPushService, register new endpoint and activate web push
    Signed-off-by: sim <git@sgougeon.fr>

---
+++ app/src/main/java/com/nextcloud/talk/services/UnifiedPushService.kt
@@ -0,0 +1,76 @@
+/*
+ * Nextcloud Talk - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Your Name <your@email.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nextcloud.talk.services
+
+import android.util.Log
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.nextcloud.talk.jobs.PushRegistrationWorker
+import com.nextcloud.talk.utils.UnifiedPushUtils.toByteArray
+import org.json.JSONException
+import org.json.JSONObject
+import org.unifiedpush.android.connector.FailedReason
+import org.unifiedpush.android.connector.PushService
+import org.unifiedpush.android.connector.data.PushEndpoint
+import org.unifiedpush.android.connector.data.PushMessage
+
+class UnifiedPushService: PushService() {
+    override fun onNewEndpoint(endpoint: PushEndpoint, instance: String) {
+        Log.d(TAG, "New endpoint for $instance")
+        val endpointBA = endpoint.toByteArray() ?: run {
+            Log.w(TAG, "Couldn't serialize endpoint!")
+            return
+        }
+        val data = Data.Builder()
+            .putString(PushRegistrationWorker.ORIGIN, "UnifiedPushService#onNewEndpoint")
+            .putLong(PushRegistrationWorker.USER_ID, instance.toLong())
+            .putByteArray(PushRegistrationWorker.UNIFIEDPUSH_ENDPOINT, endpointBA)
+            .build()
+        val pushRegistrationWork = OneTimeWorkRequest.Builder(PushRegistrationWorker::class.java)
+            .setInputData(data)
+            .build()
+        WorkManager.getInstance(this).enqueue(pushRegistrationWork)
+    }
+
+    override fun onMessage(message: PushMessage, instance: String) {
+        Log.d(TAG, "New message for $instance")
+        try {
+            val mObj = JSONObject(message.content.toString(Charsets.UTF_8))
+            val token = mObj.getString("activationToken")
+            onActivationToken(token, instance)
+        } catch (_: JSONException) {
+            // Messages are encrypted following RFC8291, and UnifiedPush lib handle the decryption itself:
+            // message.content is the cleartext
+        }
+    }
+
+    override fun onRegistrationFailed(reason: FailedReason, instance: String) {
+        Log.d(TAG, "Registration failed for $instance")
+    }
+
+    override fun onUnregistered(instance: String) {
+        Log.d(TAG, "$instance unregistered")
+    }
+
+    private fun onActivationToken(activationToken: String, instance: String) {
+        val data = Data.Builder()
+            .putString(PushRegistrationWorker.ORIGIN, "UnifiedPushService#onActivationToken")
+            .putLong(PushRegistrationWorker.USER_ID, instance.toLong())
+            .putString(PushRegistrationWorker.ACTIVATION_TOKEN, activationToken)
+            .build()
+        val pushRegistrationWork = OneTimeWorkRequest.Builder(PushRegistrationWorker::class.java)
+            .setInputData(data)
+            .build()
+        WorkManager.getInstance(this).enqueue(pushRegistrationWork)
+    }
+
+    companion object {
+        const val TAG = "UnifiedPushService"
+    }
+}
