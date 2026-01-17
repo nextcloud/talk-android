6bae963f09dfe68b1f6162a191dc5a3023b59a91
sim <git@sgougeon.fr>
2026-01-17 08:49:44 +0100

    Request notif permission with embedded distrib
    Signed-off-by: sim <git@sgougeon.fr>

--- app/src/main/java/com/nextcloud/talk/conversationlist/ConversationsListActivity.kt
+++ app/src/main/java/com/nextcloud/talk/conversationlist/ConversationsListActivity.kt
@@ -128,6 +128,7 @@ import com.nextcloud.talk.utils.Mimetype
 import com.nextcloud.talk.utils.NotificationUtils
 import com.nextcloud.talk.utils.ParticipantPermissions
 import com.nextcloud.talk.utils.SpreedFeatures
+import com.nextcloud.talk.utils.UnifiedPushUtils
 import com.nextcloud.talk.utils.UserIdUtils
 import com.nextcloud.talk.utils.bundle.BundleKeys
 import com.nextcloud.talk.utils.bundle.BundleKeys.ADD_ADDITIONAL_ACCOUNT
@@ -283,7 +284,8 @@ class ConversationsListActivity :
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
             !platformPermissionUtil.isPostNotificationsPermissionGranted() &&
             (ClosedInterfaceImpl().isGooglePlayServicesAvailable ||
-                appPreferences.useUnifiedPush)
+                appPreferences.useUnifiedPush ||
+                UnifiedPushUtils.hasEmbeddedDistributor(context))
         ) {
             requestPermissions(
                 arrayOf(Manifest.permission.POST_NOTIFICATIONS),
