5f5d4f9c3c318676a472cf4deb3393bde5757a56
rapterjet2004 <juliuslinus1@gmail.com>
2026-02-23 11:04:39 -0600

    pinned messages are now opened properly in threads
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -2545,11 +2545,10 @@ class ChatActivity :
         }
         if (position != null && position >= 0) {
             val layoutManager = binding.messagesListView.layoutManager
-            // FIXME Not a perfect offset, but works to clear the pinned message view, try and find a better solution
             (layoutManager as LinearLayoutManager).scrollToPositionWithOffset(position, 500)
         } else {
             Log.d(TAG, "message $messageId that should be scrolled to was not found (scrollToMessageWithId)")
-            startContextChatWindowForMessage(messageId, conversationThreadId?.toString())
+            startContextChatWindowForMessage(messageId, currentConversation?.internalId)
         }
     }
 
@@ -3511,7 +3510,7 @@ class ChatActivity :
 
     private fun launchFileShareLink() {
         val intent = Intent(Intent.ACTION_VIEW).apply {
-            setData((conversationUser.baseUrl + "/f/" + currentConversation?.objectId).toUri())
+            data = (conversationUser.baseUrl + "/f/" + currentConversation?.objectId).toUri()
         }
         startActivity(intent)
     }
