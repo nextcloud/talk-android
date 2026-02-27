20f9eed92b0d931f618bf18bb9df395eb85455ab
Andy Scherzinger <info@andy-scherzinger.de>
2026-02-27 20:24:34 +0100

    fix: Make top bar title single line and ellipsized
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/components/StandardAppBar.kt
+++ app/src/main/java/com/nextcloud/talk/components/StandardAppBar.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -44,7 +45,7 @@ fun StandardAppBar(
     var expanded by remember { mutableStateOf(false) }
 
     TopAppBar(
-        title = { Text(text = title) },
+        title = { Text(text = title, maxLines = 1, overflow = TextOverflow.Ellipsis) },
         colors = colors,
         navigationIcon = {
             IconButton(
