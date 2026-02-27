8af6e72dca7630e8dfc30c370ada343a3350845c
Andy Scherzinger <info@andy-scherzinger.de>
2026-02-27 11:59:20 +0100

    feat: Pass on file info to also open the file for potential editing via Files app  within the full screen activity
    Resolves #1683
    
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/fullscreenfile/FullScreenTextViewerActivity.kt
+++ app/src/main/java/com/nextcloud/talk/fullscreenfile/FullScreenTextViewerActivity.kt
@@ -8,12 +8,14 @@
  */
 package com.nextcloud.talk.fullscreenfile
 
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import androidx.fragment.app.DialogFragment
 import autodagger.AutoInjector
 import com.nextcloud.talk.BuildConfig
@@ -22,8 +24,11 @@ import com.nextcloud.talk.application.NextcloudTalkApplication
 import com.nextcloud.talk.components.ColoredStatusBar
 import com.nextcloud.talk.ui.dialog.SaveToStorageDialogFragment
 import com.nextcloud.talk.ui.theme.ViewThemeUtils
+import com.nextcloud.talk.utils.AccountUtils.canWeOpenFilesApp
 import com.nextcloud.talk.utils.Mimetype.TEXT_PREFIX_GENERIC
 import com.nextcloud.talk.utils.adjustUIForAPILevel35
+import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_ACCOUNT
+import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_FILE_ID
 import java.io.File
 import javax.inject.Inject
 
@@ -39,6 +44,10 @@ class FullScreenTextViewerActivity : AppCompatActivity() {
 
         val fileName = intent.getStringExtra("FILE_NAME").orEmpty()
         val isMarkdown = intent.getBooleanExtra("IS_MARKDOWN", false)
+        val fileId = intent.getStringExtra("FILE_ID").orEmpty()
+        val link = intent.getStringExtra("LINK")
+        val username = intent.getStringExtra("USERNAME").orEmpty()
+        val baseUrl = intent.getStringExtra("BASE_URL").orEmpty()
         val path = applicationContext.cacheDir.absolutePath + "/" + fileName
         val text = readFile(path)
 
@@ -53,9 +62,33 @@ class FullScreenTextViewerActivity : AppCompatActivity() {
                     text = text,
                     isMarkdown = isMarkdown,
                     onShare = { shareFile(path) },
-                    onSave = { showSaveDialog(fileName) }
+                    onSave = { showSaveDialog(fileName) },
+                    onOpenInFilesApp = if (fileId.isNotEmpty()) {
+                        { openInFilesApp(link, fileId, username, baseUrl) }
+                    } else {
+                        null
+                    }
                 )
             }
+        }
+    }
+
+    private fun openInFilesApp(link: String?, fileId: String, username: String, baseUrl: String) {
+        val accountString = "$username@${baseUrl.replace("https://", "").replace("http://", "")}"
+        if (canWeOpenFilesApp(this, accountString)) {
+            val filesAppIntent = Intent(Intent.ACTION_VIEW, null)
+            val componentName = ComponentName(
+                getString(R.string.nc_import_accounts_from),
+                "com.owncloud.android.ui.activity.FileDisplayActivity"
+            )
+            filesAppIntent.component = componentName
+            filesAppIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            filesAppIntent.setPackage(getString(R.string.nc_import_accounts_from))
+            filesAppIntent.putExtra(KEY_ACCOUNT, accountString)
+            filesAppIntent.putExtra(KEY_FILE_ID, fileId)
+            startActivity(filesAppIntent)
+        } else if (!link.isNullOrEmpty()) {
+            startActivity(Intent(Intent.ACTION_VIEW, link.toUri()))
         }
     }
 
