393adb80830cab3e881a7e4df7aa0f41a581148d
Andy Scherzinger <info@andy-scherzinger.de>
2026-03-01 12:35:10 +0100

    feat: Migrate open conversations list to composables
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nextcloud/talk/openconversations/ListOpenConversationsActivity.kt
+++ app/src/main/java/com/nextcloud/talk/openconversations/ListOpenConversationsActivity.kt
@@ -8,145 +8,69 @@ package com.nextcloud.talk.openconversations
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import androidx.core.graphics.drawable.toDrawable
-import androidx.core.widget.doOnTextChanged
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import autodagger.AutoInjector
-import com.google.android.material.snackbar.Snackbar
-import com.nextcloud.android.common.ui.theme.utils.ColorRole
-import com.nextcloud.talk.R
 import com.nextcloud.talk.activities.BaseActivity
-import com.nextcloud.talk.api.NcApi
 import com.nextcloud.talk.application.NextcloudTalkApplication
 import com.nextcloud.talk.chat.ChatActivity
-import com.nextcloud.talk.databinding.ActivityOpenConversationsBinding
+import com.nextcloud.talk.components.ColoredStatusBar
 import com.nextcloud.talk.models.json.conversations.Conversation
-import com.nextcloud.talk.openconversations.adapters.OpenConversationsAdapter
 import com.nextcloud.talk.openconversations.viewmodels.OpenConversationsViewModel
 import com.nextcloud.talk.utils.bundle.BundleKeys
-import com.vanniktech.ui.hideKeyboardAndFocus
-import com.vanniktech.ui.showKeyboardAndFocus
 import javax.inject.Inject
 
 @AutoInjector(NextcloudTalkApplication::class)
 class ListOpenConversationsActivity : BaseActivity() {
 
-    private lateinit var binding: ActivityOpenConversationsBinding
-
-    @Inject
-    lateinit var ncApi: NcApi
-
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    lateinit var openConversationsViewModel: OpenConversationsViewModel
-
-    lateinit var adapter: OpenConversationsAdapter
-
-    var searching = false
+    private lateinit var openConversationsViewModel: OpenConversationsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         NextcloudTalkApplication.sharedApplication!!.componentApplication.inject(this)
 
         openConversationsViewModel = ViewModelProvider(this, viewModelFactory)[OpenConversationsViewModel::class.java]
-
         openConversationsViewModel.fetchConversations()
-
-        binding = ActivityOpenConversationsBinding.inflate(layoutInflater)
-        setupActionBar()
-        setContentView(binding.root)
-        initSystemBars()
-        viewThemeUtils.platform.colorImageView(binding.searchOpenConversations, ColorRole.ON_SURFACE)
-        viewThemeUtils.material.colorTextInputLayout(binding.textInputLayout)
 
         val user = currentUserProviderOld.currentUser.blockingGet()
 
-        adapter = OpenConversationsAdapter(user, viewThemeUtils) { conversation -> adapterOnClick(conversation) }
-        binding.openConversationsRecyclerView.adapter = adapter
-        binding.searchOpenConversations.setOnClickListener {
-            searching = !searching
-            handleSearchUI(searching)
-        }
-        binding.editText.doOnTextChanged { text, _, _, count ->
-            if (!text.isNullOrBlank()) {
-                openConversationsViewModel.updateSearchTerm(text.toString())
-                openConversationsViewModel.fetchConversations()
-            } else {
-                openConversationsViewModel.updateSearchTerm("")
-                openConversationsViewModel.fetchConversations()
+        setContent {
+            val colorScheme = viewThemeUtils.getColorScheme(this)
+            val viewState by openConversationsViewModel.viewState.collectAsStateWithLifecycle()
+            val searchTerm by openConversationsViewModel.searchTerm.collectAsStateWithLifecycle()
+
+            MaterialTheme(colorScheme = colorScheme) {
+                ColoredStatusBar()
+                OpenConversationsScreen(
+                    viewState = viewState,
+                    searchTerm = searchTerm,
+                    userBaseUrl = user?.baseUrl,
+                    listenerInput = OpenConversationsScreenListenerInput(
+                        onSearchTermChange = { term ->
+                            openConversationsViewModel.updateSearchTerm(term)
+                            openConversationsViewModel.fetchConversations()
+                        },
+                        onConversationClick = { conversation -> navigateToChat(conversation) },
+                        onBackClick = { onBackPressedDispatcher.onBackPressed() }
+                    )
+                )
             }
         }
-        initObservers()
     }
 
-    private fun handleSearchUI(show: Boolean) {
-        if (show) {
-            binding.searchOpenConversations.visibility = View.GONE
-            binding.textInputLayout.visibility = View.VISIBLE
-            binding.editText.showKeyboardAndFocus()
-        } else {
-            binding.searchOpenConversations.visibility = View.VISIBLE
-            binding.textInputLayout.visibility = View.GONE
-            binding.editText.hideKeyboardAndFocus()
-        }
-    }
-
-    private fun adapterOnClick(conversation: Conversation) {
+    private fun navigateToChat(conversation: Conversation) {
         val bundle = Bundle()
         bundle.putString(BundleKeys.KEY_ROOM_TOKEN, conversation.token)
 
-        val chatIntent = Intent(context, ChatActivity::class.java)
+        val chatIntent = Intent(this, ChatActivity::class.java)
         chatIntent.putExtras(bundle)
         chatIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(chatIntent)
-    }
-
-    private fun initObservers() {
-        openConversationsViewModel.viewState.observe(this) { state ->
-            when (state) {
-                is OpenConversationsViewModel.FetchConversationsStartState -> {
-                    binding.openConversationsRecyclerView.visibility = View.GONE
-                    binding.progressBarWrapper.visibility = View.VISIBLE
-                }
-                is OpenConversationsViewModel.FetchConversationsSuccessState -> {
-                    binding.openConversationsRecyclerView.visibility = View.VISIBLE
-                    binding.progressBarWrapper.visibility = View.GONE
-                    adapter.submitList(state.conversations)
-                }
-                is OpenConversationsViewModel.FetchConversationsEmptyState -> {
-                    binding.openConversationsRecyclerView.visibility = View.GONE
-                    binding.progressBarWrapper.visibility = View.GONE
-
-                    binding.emptyList.emptyListView.visibility = View.VISIBLE
-                    binding.emptyList.emptyListViewHeadline.text = getString(R.string.nc_no_open_conversations_headline)
-                    binding.emptyList.emptyListViewText.text = getString(R.string.nc_no_open_conversations_text)
-                    binding.emptyList.emptyListIcon.setImageResource(R.drawable.baseline_info_24)
-                    binding.emptyList.emptyListIcon.visibility = View.VISIBLE
-                    binding.emptyList.emptyListViewText.visibility = View.VISIBLE
-                }
-                is OpenConversationsViewModel.FetchConversationsErrorState -> {
-                    Snackbar.make(binding.root, R.string.nc_common_error_sorry, Snackbar.LENGTH_LONG).show()
-                }
-                else -> {}
-            }
-        }
-    }
-
-    private fun setupActionBar() {
-        setSupportActionBar(binding.openConversationsToolbar)
-        binding.openConversationsToolbar.setNavigationOnClickListener {
-            if (searching) {
-                handleSearchUI(false)
-                searching = false
-            } else {
-                onBackPressedDispatcher.onBackPressed()
-            }
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setIcon(resources!!.getColor(R.color.transparent, null).toDrawable())
-        viewThemeUtils.material.themeToolbar(binding.openConversationsToolbar)
     }
 }
