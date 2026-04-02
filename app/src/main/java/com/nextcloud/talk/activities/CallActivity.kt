67a6b497061d20010643926fa3ea3b01dc4bb2dd
Tarek Loubani <tarek@tarek.org>
2026-04-02 04:32:15 -0400

    Fix call stability when backgrounding and add PIP/lifecycle diagnostic logging
    - Prevent spurious roomJoined events from re-running performCall() when
      already IN_CONVERSATION, fixing call reconnection when ChatActivity
      resumes behind PIP or task switch
    - Remove setAutoEnterEnabled(true) which conflicts with manual
      enterPictureInPictureMode() calls causing invisible PIP windows
    - Set aspect ratio in initial PIP params (onCreate) so PIP params are
      always valid
    - Add isInPipMode guard to onUserLeaveHint to prevent redundant PIP
      entry attempts
    - Add diagnostic logging to CallBaseActivity lifecycle methods and
      CallActivity PIP/call state transitions
    - Add unit tests documenting PIP race conditions and leaveRoom
      lifecycle behavior
    
    Signed-off-by: Tarek Loubani <tarek@tarek.org>

--- app/src/main/java/com/nextcloud/talk/activities/CallActivity.kt
+++ app/src/main/java/com/nextcloud/talk/activities/CallActivity.kt
@@ -756,6 +756,8 @@ class CallActivity : CallBaseActivity() {
 
     override fun onStop() {
         super.onStop()
+        Log.d(TAG, "CallActivity.onStop: isInPipMode=$isInPipMode currentCallStatus=$currentCallStatus" +
+            " isFinishing=$isFinishing isChangingConfigurations=$isChangingConfigurations")
         active = false
 
         if (isMicInputAudioThreadRunning) {
@@ -2076,15 +2078,21 @@ class CallActivity : CallBaseActivity() {
                 }
 
                 "roomJoined" -> {
-                    Log.d(TAG, "onMessageEvent 'roomJoined' joinRoomInitiated=$joinRoomInitiated")
+                    Log.d(TAG, "onMessageEvent 'roomJoined' joinRoomInitiated=$joinRoomInitiated" +
+                        " currentCallStatus=$currentCallStatus")
                     if (!joinRoomInitiated) {
                         Log.d(TAG, "Ignoring stale roomJoined event (joinRoomAndCall not yet called)")
                         return
                     }
                     startSendingNick()
                     if (webSocketCommunicationEvent.getHashMap()!!["roomToken"] == roomToken) {
-                        roomJoinRefreshes = 0
-                        performCall()
+                        if (currentCallStatus === CallStatus.IN_CONVERSATION) {
+                            Log.d(TAG, "Already in conversation, skipping performCall()" +
+                                " (ChatActivity resume triggered spurious roomJoined)")
+                        } else {
+                            roomJoinRefreshes = 0
+                            performCall()
+                        }
                     }
                 }
 
@@ -3239,8 +3247,8 @@ class CallActivity : CallBaseActivity() {
 
     override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
         super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
-        Log.d(TAG, "onPictureInPictureModeChanged")
-        Log.d(TAG, "isInPictureInPictureMode= $isInPictureInPictureMode")
+        Log.d(TAG, "onPictureInPictureModeChanged: isInPictureInPictureMode=$isInPictureInPictureMode" +
+            " currentCallStatus=$currentCallStatus isIntentionallyLeavingCall=$isIntentionallyLeavingCall")
         isInPipMode = isInPictureInPictureMode
         if (isInPictureInPictureMode) {
             mReceiver = object : BroadcastReceiver() {
