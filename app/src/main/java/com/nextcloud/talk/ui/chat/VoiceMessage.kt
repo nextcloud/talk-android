904cd45e43e89ecf278e5cbac1863605f8b098b2
rapterjet2004 <juliuslinus1@gmail.com>
2026-04-07 11:40:31 -0500

    Compose waveform seekbar
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

--- app/src/main/java/com/nextcloud/talk/ui/chat/VoiceMessage.kt
+++ app/src/main/java/com/nextcloud/talk/ui/chat/VoiceMessage.kt
@@ -8,7 +8,6 @@
 package com.nextcloud.talk.ui.chat
 
 import android.text.format.DateUtils
-import android.widget.SeekBar
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,26 +18,32 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import com.nextcloud.talk.R
 import com.nextcloud.talk.chat.ui.model.ChatMessageUi
 import com.nextcloud.talk.chat.ui.model.MessageTypeContent
-import com.nextcloud.talk.ui.WaveformSeekBar
+import com.nextcloud.talk.ui.ComposeWaveformSeekBar
+import com.nextcloud.talk.ui.WAVEFORM_SIZE
+import com.nextcloud.talk.utils.AudioUtils
 
 private const val SEEKBAR_MAX = 100
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Suppress("Detekt.LongMethod", "LongParameterList")
 @Composable
 fun VoiceMessage(
@@ -57,12 +62,14 @@ fun VoiceMessage(
         forceTimeBelow = true,
         content = {
             val inversePrimaryColor = colorScheme.inversePrimary
-            val inversePrimary = remember(inversePrimaryColor) { inversePrimaryColor.toArgb() }
+            remember(inversePrimaryColor) { inversePrimaryColor.toArgb() }
             val onPrimaryContainerColor = colorScheme.onPrimaryContainer
-            val onPrimaryContainer = remember(onPrimaryContainerColor) { onPrimaryContainerColor.toArgb() }
+            remember(onPrimaryContainerColor) { onPrimaryContainerColor.toArgb() }
             val remainingSeconds = (typeContent.durationSeconds - typeContent.playedSeconds).coerceAtLeast(0)
-            val waveformData = remember(typeContent.waveform) { typeContent.waveform.toFloatArray() }
-            val lastWaveformData = remember { mutableListOf<Float>() }
+            val waveformData = remember(typeContent.waveform) {
+                val floatArr = typeContent.waveform.toFloatArray()
+                AudioUtils.shrinkFloatArray(floatArr, WAVEFORM_SIZE)
+            }
 
             Column {
                 Row(
@@ -84,49 +91,21 @@ fun VoiceMessage(
                         }
                     }
 
-                    AndroidView(
-                        factory = { ctx ->
-                            WaveformSeekBar(ctx).apply {
-                                max = SEEKBAR_MAX
-                                setWaveData(waveformData)
-                                setColors(
-                                    inversePrimary,
-                                    onPrimaryContainer
-                                )
-                                setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                                    override fun onProgressChanged(
-                                        seekBar: SeekBar?,
-                                        progress: Int,
-                                        fromUser: Boolean
-                                    ) {
-                                        if (fromUser) {
-                                            onSeek(message.id, progress)
-                                        }
-                                    }
+                    var sliderValue by remember { mutableFloatStateOf(0f) }
+                    sliderValue = typeContent.seekbarProgress * 1f / SEEKBAR_MAX
 
-                                    override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
-
-                                    override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
-                                })
-                            }
-                        },
-                        update = { seekBar ->
-                            seekBar.max = SEEKBAR_MAX
-                            val waveformChanged = typeContent.waveform != lastWaveformData
-                            if (waveformChanged) {
-                                lastWaveformData.clear()
-                                lastWaveformData.addAll(typeContent.waveform)
-                                seekBar.setWaveData(waveformData)
-                                seekBar.requestLayout()
-                            }
-                            seekBar.setColors(inversePrimary, onPrimaryContainer)
-                            seekBar.progress = typeContent.seekbarProgress
-                            seekBar.isEnabled = !typeContent.isDownloading
-                            seekBar.invalidate()
+                    ComposeWaveformSeekBar(
+                        sliderValue,
+                        {
+                            val progressI = (it * SEEKBAR_MAX).toInt()
+                            onSeek(message.id, progressI)
+                            sliderValue = it
                         },
                         modifier = Modifier
-                            .weight(1f)
                             .height(56.dp)
+                            .fillMaxWidth()
+                            .padding(8.dp), // or weight(1f),
+                        waveformData
                     )
 
                     TextButton(
