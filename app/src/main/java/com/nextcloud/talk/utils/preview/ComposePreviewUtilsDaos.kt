db56d2b399dbe92364a848c2be549e98d6cec7eb
Marcel Hibbe <dev@mhibbe.de>
2026-06-12 14:30:28 +0200

    fix expiring messages
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/src/main/java/com/nextcloud/talk/utils/preview/ComposePreviewUtilsDaos.kt
+++ app/src/main/java/com/nextcloud/talk/utils/preview/ComposePreviewUtilsDaos.kt
@@ -132,6 +132,8 @@ class DummyChatMessagesDaoImpl : ChatMessagesDao {
         /* */
     }
 
+    override suspend fun deleteExpiredMessages(internalConversationId: String, currentTimeSecs: Long): Int = 0
+
     override fun getNumberOfThreadReplies(internalConversationId: String, threadId: Long): Int = 0
 }
 
@@ -284,5 +286,10 @@ class DummyChatBlocksDaoImpl : ChatBlocksDao {
     override fun deleteChatBlocksOlderThan(internalConversationId: String, messageId: Long) {
         /* */
     }
+
+    override suspend fun deleteAllChatBlocksForConversation(internalConversationId: String) {
+        /* */
+    }
+
     override fun getLatestChatBlock(internalConversationId: String, threadId: Long?): Flow<ChatBlockEntity?> = flowOf()
 }
