2b6a20f6576b5ae43d81ff33d21735bd24f6faf5
rapterjet2004 <juliuslinus1@gmail.com>
2026-07-14 12:51:20 -0500

    Refactoring message drafts to use new source of truth fixing previous unsynchronized state bug
    moving away from vanniktech emoji picker
    
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

--- app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
+++ app/src/main/java/com/nextcloud/talk/chat/ChatActivity.kt
@@ -4155,15 +4155,11 @@ class ChatActivity :
     }
 
     fun cancelReply() {
-        messageInputViewModel.reply(null)
-        chatViewModel.messageDraft.quotedMessageText = null
-        chatViewModel.messageDraft.quotedDisplayName = null
-        chatViewModel.messageDraft.quotedImageUrl = null
-        chatViewModel.messageDraft.quotedJsonId = null
+        messageInputViewModel.cancelReply()
     }
 
     fun cancelCreateThread() {
-        chatViewModel.clearThreadTitle()
+        messageInputViewModel.cancelCreateThread()
     }
 
     companion object {
