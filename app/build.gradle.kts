fbcaaeccfdd98a767bf5f3db50ba5031410b3dcd
Marcel Hibbe <dev@mhibbe.de>
2026-08-20 16:42:02 +0200

    bump version to 24.0.3
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
 
         // mayor.minor.hotfix.increment (for increment: 01-50=Alpha / 51-89=RC / 90-99=stable)
         // xx   .xxx  .xx    .xx
-        versionCode = 240000092
-        versionName = "24.0.2"
+        versionCode = 240000093
+        versionName = "24.0.3"
 
         vectorDrawables.useSupportLibrary = true
 
