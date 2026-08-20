c0d961ca1bd7f4569613359be6f4f7a88b167793
Marcel Hibbe <dev@mhibbe.de>
2026-08-20 20:22:58 +0200

    bump version to 25.0.0 RC1
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
 
         // mayor.minor.hotfix.increment (for increment: 01-50=Alpha / 51-89=RC / 90-99=stable)
         // xx   .xxx  .xx    .xx
-        versionCode = 240010021
-        versionName = "24.1.0 Alpha 21"
+        versionCode = 250000051
+        versionName = "25.0.0 RC1"
 
         vectorDrawables.useSupportLibrary = true
 
