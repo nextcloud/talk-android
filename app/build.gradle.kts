0819c3350973c14541cac5afde8add4ff52f1947
Marcel Hibbe <dev@mhibbe.de>
2026-08-24 11:15:01 +0200

    bump version to 24.0.4
    no new fixes. 24.0.3 seemed to be released with the wrong build flavor on gplay. So 24.0.4 will fix this
    
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
 
         // mayor.minor.hotfix.increment (for increment: 01-50=Alpha / 51-89=RC / 90-99=stable)
         // xx   .xxx  .xx    .xx
-        versionCode = 240000093
-        versionName = "24.0.3"
+        versionCode = 240000094
+        versionName = "24.0.4"
 
         vectorDrawables.useSupportLibrary = true
 
