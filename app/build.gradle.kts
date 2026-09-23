db55ade3887abdc07d27a589140c2186a1134297
Marcel Hibbe <dev@mhibbe.de>
2026-09-23 17:12:33 +0200

    remove kotlinx-coroutines-rx2
    Signed-off-by: Marcel Hibbe <dev@mhibbe.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -211,7 +211,6 @@ dependencies {
     implementation(fileTree(mapOf("include" to listOf("*"), "dir" to "libs")))
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$coroutinesVersion")
 
     implementation("androidx.appcompat:appcompat:1.8.0")
     implementation("com.google.android.material:material:1.14.0")
